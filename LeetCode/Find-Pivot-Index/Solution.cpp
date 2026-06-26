38c69dc85e5deb9d185ea1188ab6dc8ef776d908
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-06-26 07:04:48 +0530

    Add solution for Find Pivot Index

--- LeetCode/Find-Pivot-Index/Solution.cpp
+++ LeetCode/Find-Pivot-Index/Solution.cpp
@@ -1,24 +1,18 @@
 1class Solution {
 2public:
-3int sumfun(vector<int>&nums,int n){
-4    int sum=0;
-5    for(int i=0;i<n;i++){
-6        sum+=nums[i];
-7    }
-8    return sum;
-9}
-10    int pivotIndex(vector<int>& nums) {
-11        int n=nums.size();
-12        int sum=sumfun(nums,n);
-13        int left=0;
-14        int right=0;
-15        for (int i=0;i<n;i++){
-16          right=sum-left-nums[i];
-17          if(left==right){
-18            return i;
-19          }
-20           left+=nums[i];
-21        }
-22        return -1;
-23    }
-24};
+3    int pivotIndex(vector<int>& nums) {
+4        int left=0;
+5        int right=0;
+6        int n=nums.size();
+7        int sum=0;
+8        for(int i=0;i<n;i++){
+9            sum+=nums[i];
+10        }
+11        for(int i=0;i<n;i++){
+12          right=sum-left-nums[i];
+13          if(left==right)return i;
+14          left+=nums[i];
+15        }
+16        return -1;
+17    }
+18};
