b920fc667c7e3df12c28e099e59d0bbbf9118ceb
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-06-26 16:54:39 +0530

    Add solution for Find the Duplicate Number

--- LeetCode/Find-the-Duplicate-Number/Solution.cpp
+++ LeetCode/Find-the-Duplicate-Number/Solution.cpp
@@ -1,23 +1,21 @@
 1class Solution {
 2public:
 3    int findDuplicate(vector<int>& nums) {
-4         // Let's use slow-fast apporach here(main tax how to traverse)
-5         int slow=0;
-6         int fast=0;
-7         while(true){    // no need to check becuase given hai question mai ki given hai ek duplicate for sure
-8            slow=nums[slow];
+4        int slow=0;
+5        int fast=0;
+6        while(true ){
+7            slow=nums[slow];
+8            fast=nums[fast];
 9            fast=nums[fast];
-10            fast=nums[fast];  // two times
-11            if(slow==fast){
-12                slow=0;
-13                while(slow!=fast){
-14                    slow=nums[slow];
-15                    fast=nums[fast];
-16                }
-17                return slow;    // yahi humara duplicate point hoga for sure
-18            }
-19
-20         }
-21         return -1;
-22    }
-23};
+10            if(slow==fast){
+11                slow=0;
+12                while(slow!=fast){
+13                    slow= nums[slow];
+14                    fast=nums[fast];
+15                }
+16                return slow;
+17            }
+18        }
+19        return -1;
+20    }
+21};
