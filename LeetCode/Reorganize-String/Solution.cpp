ef96947e2e5408bc3d43987e971ae94b986c8ace
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-06-27 16:06:39 +0530

    Add solution for Reorganize String

--- LeetCode/Reorganize-String/Solution.cpp
+++ LeetCode/Reorganize-String/Solution.cpp
@@ -1,58 +1,54 @@
 1class Solution {
 2public:
-3struct emp{
-4    bool operator()(pair<int,char>&a,pair<int,char>&b){
-5        if(a.first!=b.first){
-6            return a.first<b.first;
-7        }
-8        return a.second<b.second;
-9    }
-10};
-11    string reorganizeString(string s) {
-12        priority_queue<pair<int,char>,vector<pair<int,char>>,emp>pq;
-13        //bUILD Hashmap
-14        unordered_map<char,int>mpp1;
-15        for(int i=0;i<s.size();i++){
-16            mpp1[s[i]]++;
-17        }
-18        // now insert all this in heap
-19        for(auto i:mpp1){
-20            int freq=i.second;
-21            int character=i.first;
-22            pair<int,char>curr={freq,character};
-23            pq.push(curr);
-24        }
-25        // now we have to make result
-26        
-27        // vector<string>res;
+3//MAX HEAP COMPATOR
+4struct emp{
+5    bool operator()(pair<int,char>&a,pair<int,char>&b){
+6        if(a.first!=b.first){
+7            return a.first<b.first;
+8        }
+9        return a.second<b.second;
+10    }
+11};
+12    string reorganizeString(string s) {
+13        int n=s.size();
+14        // max heap
+15        priority_queue<pair<int,char>,vector<pair<int ,char>> ,emp>pq;
+16        // unordered_map for storing frequencies
+17        unordered_map<char,int>mpp1;
+18        for(int i=0;i<n;i++){
+19            mpp1[s[i]]++;
+20        }
+21        // now it;s time to push in pq
+22        for(auto i:mpp1){
+23            char ch=i.first;
+24            int freq=i.second;
+25            pair<int ,char>curr={freq,ch};
+26            pq.push(curr);
+27        }
 28        string res="";
-29         int seat=0;
-30        while(!pq.empty()){ 
-31           pair<int,char>p=pq.top();
-32           pq.pop();
-33            if(res.size()==0 || res[seat-1] != p.second){
-34                res.push_back(p.second);
-35                seat++;
-36                p.first--;
-37                if(p.first>0){
-38                pq.push(p);
-39                }
-40            }
-41            else{
-42                if(pq.empty()){return "";}
-43           pair<int,char>p1=pq.top();
-44           pq.pop();
-45        //    if(p1.second==res[seat-1]) return "";
-46           res.push_back(p1.second);
-47           seat++;
-48           p1.first--;
-49           if(p1.first>0){
-50            pq.push(p1);
-51           }
-52           pq.push(p);
-53            }
-54        }
-55           return res;
-56            }
-57
-58};
+29        int idx=0;
+30        while(!pq.empty()){
+31             pair<int,char>top1=pq.top();
+32             pq.pop();
+33             if(idx==0 || res[idx-1]!=top1.second){
+34                res.push_back(top1.second);
+35                idx++;
+36                top1.first--;
+37                if(top1.first>0)pq.push({top1.first,top1.second});
+38                continue;
+39             }
+40             else{
+41                if(pq.empty())return "";
+42                pair<int,char>top2=pq.top();
+43                pq.pop();
+44                res.push_back(top2.second);
+45                idx++;
+46                top2.first--;
+47                if(top2.first>0)pq.push({top2.first,top2.second});
+48                pq.push(top1);
+49             }
+50             
+51        }
+52      return res;  
+53    }
+54};
