e1758b2d3e2d9847cda67524b4b0f6a771e678b3
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-06-30 07:03:02 +0530

    Add solution for Daily Temperatures

--- LeetCode/Daily-Temperatures/Solution.cpp
+++ LeetCode/Daily-Temperatures/Solution.cpp
@@ -1,26 +1,22 @@
 1class Solution {
 2public:
 3    vector<int> dailyTemperatures(vector<int>& temperatures) {
-4        // int count=1;
-5        int n=temperatures.size();
-6        vector<int>res(n);
-7        stack<int >st;
-8        st.push(n-1);
-9        res[n-1]=0;
-10        for(int i=n-2;i>=0;i--){
-11            while(!st.empty() &&  temperatures[st.top()]<=temperatures[i]){
-12                st.pop();
-13                // count++;
-14            }
-15            if(st.empty()){
-16                res[i]=0;
-17            }
-18            else{
-19                res[i]=st.top()-i;    // no
-20            }
-21             st.push(i);
-22        }
-23        return res;
-24    }
-25};
-26
+4       int n=temperatures.size();
+5       stack<int>st;
+6       vector<int>res(n);
+7       st.push(n-1);
+8       res[n-1]=0;
+9       for(int i=n-2;i>=0;i--){
+10        while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
+11            st.pop();
+12        }
+13        if(st.empty())res[i]=0;
+14        else{
+15            res[i]=st.top()-i;
+16        }
+17        st.push(i);
+18       }
+19       return res;
+20    }
+21};
+22 // we are going to use next greater element logic but we are storing indexes instead of actual value
