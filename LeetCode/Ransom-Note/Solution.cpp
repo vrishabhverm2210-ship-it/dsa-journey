30216a9beffd5f004264d537e295b37e1ba80760
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-06-30 08:25:01 +0530

    Add solution for Ransom Note

--- LeetCode/Ransom-Note/Solution.cpp
+++ LeetCode/Ransom-Note/Solution.cpp
@@ -1,26 +1,23 @@
 1class Solution {
 2public:
 3    bool canConstruct(string ransomNote, string magazine) {
-4         map<char,int>mpp1;
-5         for(int i=0;i<magazine.size();i++){
-6            mpp1[magazine[i]]++;
-7         }
-8
-9         // now scan ransomNote  (Direct check ho ra hai iss method se cool hai yeh map vala method , map ki indexing ko utilise krna sikho) 
-10           for (char c : ransomNote) {
-11            if (mpp1[c] > 0) {  // check if available
-12                mpp1[c]--;     // use it
-13            } else {
-14                return false;  // not enough letters
-15            }
-16        }
-17            // don't use find vala method , instead use above method
-18            // if(mpp1.find(ransomNote[i])!= mpp1.end()){
-19            // count++;
-20            // mpp1[ransomNote[i]]--;
-21            // }
-22         
-23      
-24   return true;
-25    }
-26};
+4        // make two maps and compare
+5        unordered_map<char,int>need;
+6        for(int i=0;i<ransomNote.size();i++){
+7            need[ransomNote[i]]++;
+8        }
+9          unordered_map<char,int>have;
+10        for(int i=0;i<magazine.size();i++){
+11            have[magazine[i]]++;
+12        }
+13        // now for compare
+14        for(auto i:need){
+15            char ch=i.first;
+16            int freq1=i.second;
+17            int freq2=have[ch];
+18
+19            if(freq1>freq2)return false;
+20        }
+21return true;
+22    }
+23};
