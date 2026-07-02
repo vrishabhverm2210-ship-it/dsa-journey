e8f9d62f3a17cfa0c5220db8cbe7354bb4f88eb4
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-02 07:31:55 +0530

    Add solution for Reverse Linked List II

--- LeetCode/Reverse-Linked-List-II/Solution.cpp
+++ LeetCode/Reverse-Linked-List-II/Solution.cpp
@@ -11,36 +11,36 @@
 11class Solution {
 12public:
 13    ListNode* reverseBetween(ListNode* head, int left, int right) {
-14        // firstly write base cases
-15        if(left==right)return head;
-16        if(head==NULL) return NULL;
-17         ListNode* t=head;
-18         int pos=1;
-19          ListNode* begin=NULL;
-20
-21        while(pos < left){
-22           begin = t;
-23           t = t->next;
-24           pos++;
-25}
-26         
-27         int times=right-left+1;
-28         // once hum rev valli site pe pahuch gaye
-29          ListNode* curr=t;
-30           ListNode* prev=NULL;
-31           while(times--){ // NO of times reverse process should taken place
-32          ListNode* next= curr->next;
-33        curr->next=prev;
-34        prev=curr;
-35        curr=next;
-36           }
-37           // now the part we have to reverse is done(now we have to just link the connections)
-38           t->next=curr;
-39           if(begin){
-40           begin->next=prev;
-41           return head;
-42           }
-43           return prev;
+14        if(left==right)return head;
+15        if(head==NULL )return NULL;
+16        int count=1;
+17        ListNode *t=head;
+18        ListNode *begin=NULL;
+19        while(count<left){
+20            begin=t;
+21            t=t->next;
+22            count++;
+23        }
+24        // now we have reached to the proper position for reversal
+25        int times=right-left+1 ; // prev ki position ke hisab se
+26         ListNode *curr=t;
+27         ListNode *prev=NULL;
+28         while(times--){
+29            // if(curr==NULL)break;
+30             ListNode *next=curr->next;
+31            
+32             curr->next=prev;
+33             prev=curr;
+34             curr=next;
+35         }
+36
+37         // now we just have to do the connections
+38         t->next=curr;
+39         if(begin){
+40            begin->next=prev;
+41            return head;
+42         }
+43         return prev;
 44
 45    }
 46};
