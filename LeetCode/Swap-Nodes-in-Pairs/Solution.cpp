405afd6242ea96ea2d96bbf1951eac310ccd4dea
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-02 08:02:21 +0530

    Add solution for Swap Nodes in Pairs

--- LeetCode/Swap-Nodes-in-Pairs/Solution.cpp
+++ LeetCode/Swap-Nodes-in-Pairs/Solution.cpp
@@ -10,57 +10,53 @@
 10 */
 11class Solution {
 12public:
-13void reverse(ListNode* head,int times){
-14    ListNode* curr=head;
-15    ListNode* prev=NULL;
+13void reverse(ListNode *head,int times){
+14    ListNode *curr=head;
+15    ListNode *prev=NULL;
 16    while(times--){
-17        ListNode * next=curr->next;
+17        ListNode *next=curr->next;
 18        curr->next=prev;
 19        prev=curr;
 20        curr=next;
 21    }
 22}
 23    ListNode* swapPairs(ListNode* head) {
-24        if(head==NULL)return NULL;
-25        ListNode* left=head;
-26        ListNode* right=NULL;
-27        int size=2;
-28        ListNode* prevleft=NULL;
-29        ListNode* res=NULL;
-30        while(true){
-31            right= left;
-32            for(int i=0;i<size-1;i++){
-33                if(right==NULL){
-34                break;
+24          ListNode *left=head;
+25            ListNode *right=NULL;
+26         ListNode *prevleft=NULL;
+27         int size=2;
+28         ListNode *res=NULL;
+29         while(true){
+30             right=left;
+31         for(int i=0;i<size-1;i++){
+32            if(right==NULL){
+33            // right=NULL;
+34            break;  // remember the last one case when there is no right
 35            }
-36                right=right->next;
-37            }
-38            // firstly store next left
-39            if(right){
-40            ListNode * nextleft=right->next;
-41            reverse(left,size);
-42              if(prevleft){
-43                prevleft->next=right;     
-44              }
-45            prevleft=left;
-46              if(res==NULL)res=right; // storing the first one
+36            right=right->next;
+37         }
+38         if(right){
+39             ListNode *nextleft=right->next;
+40             reverse(left,2);
+41             if(prevleft){
+42                prevleft->next=right;
+43             }
+44             prevleft=left;
+45             if(res==NULL)res=right;
+46             
 47
-48              left=nextleft;
-49            }
-50            else{// right na aa paya (single node left)
+48             left=nextleft;
+49         }
+50         else{
 51            if(prevleft){
-52                prevleft->next=left; // curr vale left ko prevleft se jod dia 
-53            }
-54
-55                if(res==NULL)res=left;  // if single node hoti
-56
-57                break;
-58            }
-59         }
-60                 
-61        
-62
-63        return res;
-64        
-65    }
-66};
+52                prevleft->next=left;
+53                break;
+54                }
+55            
+56            if(res==NULL)res=left; 
+57            break;  
+58         }
+59       }
+60         return res;
+61    }
+62};
