59c276c406b64d480e8b2e2c449bdbaf0afd79b2
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-02 12:05:24 +0530

    Add solution for Subarray Sum Equals K

--- LeetCode/Subarray-Sum-Equals-K/Solution.cpp
+++ LeetCode/Subarray-Sum-Equals-K/Solution.cpp
@@ -1,16 +1,2 @@
-1class Solution {
-2public:
-3    int subarraySum(vector<int>& nums, int k) {
-4        int sum=0;
-5        unordered_map<int,int>mpp1;
-6        int res=0;
-7        mpp1[0]=1; // ,must codntion
-8        for(int i=0;i<nums.size();i++){
-9            sum+=nums[i];
-10            int freq=mpp1[sum-k];
-11            res+=freq;
-12            mpp1[sum]++;
-13        }
-14        return res;
-15    }
-16};
+sum(A[:j]) = sum(A[:i]) + k 
+sum(A[:j]) - k = sum(A[:i])
