799968cdb3106caf0d6e17a9d83623fb7d5f84c1
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-02 09:26:17 +0530

    Add solution for Maximum Subarray Sum with One Deletion

--- LeetCode/Maximum-Subarray-Sum-with-One-Deletion/Solution.cpp
+++ LeetCode/Maximum-Subarray-Sum-with-One-Deletion/Solution.cpp
@@ -1,24 +1,26 @@
 1class Solution {
 2public:
 3    int maximumSum(vector<int>& arr) {
-4        int onedelete=INT_MIN;
-5        int nodelete=arr[0];
-6        int res=arr[0];
-7        int v;
+4        // IN THIS QUESTION WE HAVE TWO CHOICES SO MAKE TO VARIABLE
+5        int onedelete=INT_MIN;
+6        int nodelete=arr[0];
+7        int res=arr[0];
 8        for(int i=1;i<arr.size();i++){
 9            int prevnodelete=nodelete;
-10            int prevdelete=onedelete;
-11            nodelete=max(prevnodelete+arr[i], arr[i]);
-12            if(prevdelete==INT_MIN){
-13                v=arr[i];   // means let's start with deleting this itself
-14            }
-15            else{
-16                v=prevdelete+arr[i];
+10            int prevonedelete=onedelete;
+11            // now choices if we still don't want to delete
+12            nodelete=max(prevnodelete+arr[i],arr[i]);// out of 4 choices 2 is used here
+13            // for remaining two
+14            int v;
+15            if(prevonedelete==INT_MIN){
+16                v=arr[i];
 17            }
-18            onedelete=max(v,prevnodelete);//+ delete hora hai yeh current vala);
-19           res=max({res,onedelete,nodelete});
-20        }
-21        return res;
-22    }
-23};
-24
+18            else{
+19                v= prevonedelete+arr[i];
+20            }
+21            onedelete=max(v,prevnodelete);
+22            res=max({res,nodelete,onedelete});
+23        }
+24        return res;
+25    }
+26};
