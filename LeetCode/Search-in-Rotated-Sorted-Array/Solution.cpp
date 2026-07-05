e8cad5d5c8ad31d029ea70737d4d2241bc163812
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-05 07:44:40 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
@@ -1,66 +1,44 @@
 1class Solution {
 2public:
-3    int findpivot(const vector<int>& nums, int n) {
-4        int s = 0;
-5        int e = n - 1;
-6        // If array is already sorted (no rotation)
-7        if (nums[s] <= nums[e]) {
-8            return 0;
-9        }
-10        while (s < e) {
-11            int mid = s + (e - s) / 2;
-12            if (nums[mid] > nums[e]) {
-13                // pivot is to right of mid
-14                s = mid + 1;
-15            } else {
-16                // pivot is at mid or to left of mid
-17                e = mid;
-18            }
-19        }
-20        return s;
-21    }
-22
-23    int binarySearch(const vector<int>& nums, int start, int end, int target) {
-24        int s = start;
-25        int e = end;
-26        while (s <= e) {
-27            int mid = s + (e - s) / 2;
-28            if (nums[mid] == target) {
-29                return mid;
-30            } else if (nums[mid] > target) {
-31                e = mid - 1;
-32            } else {
-33                s = mid + 1;
-34            }
-35        }
-36        return -1;
-37    }
-38
-39    int search(vector<int>& nums, int target) {
-40        int n = nums.size();
-41        if (n == 0) return -1;
-42
-43        int pivot = findpivot(nums, n);
-44
-45        // if pivot element is the target
-46        if (nums[pivot] == target) {
-47            return pivot;
-48        }
-49
-50       if (pivot == 0) {
-51    return binarySearch(nums, 0, n - 1, target);
-52}
-53
-54if (target >= nums[0]) {
-55    // target in left sorted part
-56    return binarySearch(nums, 0, pivot - 1, target);
-57} else {
-58    // target in right sorted part
-59    return binarySearch(nums, pivot, n - 1, target);
-60}
-61
-62}
-63    
-64};
-65
-66
+3    int search(vector<int>& nums, int target) {
+4        int n=nums.size();
+5        int low=0;
+6        int high=n-1;
+7        while(low<=high){
+8            int mid=low+(high-low)/2;
+9            if(nums[mid]>nums[n-1]){// 2nd part
+10                if(nums[mid]==target)return mid;
+11                else if(nums[mid]<target){
+12                    low=mid+1;
+13                }
+14                else{  // two choices(we have to decidde by target itself)
+15
+16                if(target>nums[n-1]){  // means target lies in second part
+17                    high=mid-1;
+18
+19                }
+20                else{
+21                    low=mid+1;
+22                }
+23                }
+24            }
+25            else{
+26                if(nums[mid]==target){
+27                    return mid;
+28                }
+29                else if(nums[mid]>target){
+30                    high=mid-1;
+31                }
+32                else{
+33                    if(target>nums[n-1]){
+34                        high=mid-1;
+35                    }
+36                    else{
+37                        low=mid+1;
+38                    }
+39                }
+40            }
+41        }
+42        return -1;
+43    }
+44};
