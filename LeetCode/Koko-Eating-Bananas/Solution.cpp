a4f4b06ddca5f9ca048be86ff0ed11356508f835
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-05 07:31:33 +0530

    Add solution for Koko Eating Bananas

--- LeetCode/Koko-Eating-Bananas/Solution.cpp
+++ LeetCode/Koko-Eating-Bananas/Solution.cpp
@@ -1,38 +1,38 @@
 1class Solution {
 2public:
-3int maxi(vector<int>& piles){
+3int maxi(vector<int>& piles ){
 4    int maxi1=INT_MIN;
 5    for(int i=0;i<piles.size();i++){
-6        maxi1=max(maxi1,piles[i]);
+6        maxi1=max(piles[i],maxi1);
 7    }
 8    return maxi1;
 9}
 10    int minEatingSpeed(vector<int>& piles, int h) {
-11       int res;  // FOR STORING THE FINAL RESULT
-12        int low=1;
-13        int high=maxi(piles);
-14        while(low<=high){
-15            int mid=low+(high-low)/2;  // use this middle no
-16             long long speed=0;  // for every iteration it will start new
-17            for(int i=0;i<piles.size();i++){
-18                if(piles[i]%mid==0){
-19                   speed+=piles[i]/mid;
-20                } 
-21                else{
-22                    speed+=piles[i]/mid;
-23                    speed+=1;
-24                }
-25            }
-26
-27            if(speed>h){   // means guard will come before completing bananas
-28               low=mid+1;
-29            }
-30            else {
-31              res=mid;
-32              high=mid-1;  // search for less 
+11        int low=1;
+12        int high=maxi(piles);
+13         int res=-1;
+14         while(low<=high){
+15            int mid=low+(high-low)/2;
+16            // find the speed
+17            long long speed=0;
+18            for(int i=0;i<piles.size();i++){
+19                if(piles[i]%mid ==0){
+20                    speed+=piles[i]/mid;
+21                }
+22                else{
+23                    speed+=piles[i]/mid;
+24                    speed++;
+25                }
+26            }
+27            if(speed<=h){
+28                res=mid;
+29                high=mid-1;
+30            }
+31            else{
+32                low=mid+1;
 33            }
-34            
-35        }
-36return res;
+34
+35         }
+36         return res;
 37    }
 38};
