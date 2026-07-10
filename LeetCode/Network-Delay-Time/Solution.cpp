03e417b22383a7573e1cf0797d97feafa46b951c
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-10 08:18:20 +0530

    Add solution for Network Delay Time

--- LeetCode/Network-Delay-Time/Solution.cpp
+++ LeetCode/Network-Delay-Time/Solution.cpp
@@ -1,43 +1,42 @@
 1class Solution {
-2public:   // Minimum time means jo time sbse bda hoga res vector mai vo hoga main anser
+2public:
 3    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-4        // make adj list
+4        // make the aj list
 5        vector<vector<pair<int,int>>>adj(n);
 6        for(int i=0;i<times.size();i++){
-7            int source=times[i][0];
+7            int src=times[i][0];
 8            int dest=times[i][1];
 9            int wt=times[i][2];
-10            // now it is given that indexing is 1 to n
-11            adj[source-1].push_back({dest-1,wt});  
-12                    }
-13     vector<int>res(n,INT_MAX);
-14     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-15     res[k-1]=0; // must step
-16     pq.push({0,k-1});
-17     while(!pq.empty()){
-18        pair<int,int>top=pq.top();
-19        pq.pop();
-20        int dist=top.first;
-21        int node=top.second;
-22        // pruning cdn
-23        if(dist>res[node])continue;
-24        for(int i=0;i<adj[node].size();i++){
-25            int neigh=adj[node][i].first;
-26            int wt=adj[node][i].second;
-27            // relaxation conditiom
-28            if(wt+dist < res[neigh]){
-29                res[neigh]=wt+dist;
-30                pq.push({wt+dist,neigh});
-31            }
-32        }
-33     }
-34// now we have to return the max answer and if not posiible so return --1
-35int maxi=INT_MIN;
-36for(int i=0;i<n;i++){
-37    if(res[i]==INT_MAX)return -1;
-38    maxi=max(res[i],maxi);
-39}
-40return maxi;
-41
-42    }
-43};
+10            // as it is directed
+11            adj[src-1].push_back({dest-1,wt});
+12        }
+13        // make the priority queue
+14        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+15        pq.push({0,k-1});
+16        vector<int>res(n,1e8);
+17        res[k-1]=0;
+18        while(!pq.empty()){
+19            pair<int,int>top=pq.top();
+20            pq.pop();
+21            int dist=top.first;
+22            int node=top.second;
+23            if(dist>res[node])continue;
+24            for(int i=0;i<adj[node].size();i++){
+25                pair<int,int> top1=adj[node][i];
+26                int neigh=top1.first;
+27                int wt=top1.second;
+28                if(wt+dist<res[neigh]){
+29                    res[neigh]=wt+dist;
+30                    pq.push({wt+dist,neigh});
+31                }
+32            }
+33        }
+34        // find the maximum one
+35        int maxi=INT_MIN;
+36        for(int i=0;i<res.size();i++){
+37            maxi=max(maxi,res[i]);
+38        }
+39        if(maxi==1e8)return -1;
+40        return maxi;
+41    }
+42};
