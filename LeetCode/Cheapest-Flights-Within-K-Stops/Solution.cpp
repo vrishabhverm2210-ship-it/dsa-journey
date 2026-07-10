51a1e795405dfaa701cf3d76502837f4acd9fe31
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-10 14:02:35 +0530

    Add solution for Cheapest Flights Within K Stops

--- LeetCode/Cheapest-Flights-Within-K-Stops/Solution.cpp
+++ LeetCode/Cheapest-Flights-Within-K-Stops/Solution.cpp
@@ -1,24 +1,22 @@
 1class Solution {
 2public:
 3    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-4        // let's do this with belman algo 
-5        vector<int>res(n,1e8);
-6        res[src]=0;
+4         vector<int>res(n,1e9);
+5         res[src]=0;
+6         vector<int>temp;
 7        for(int i=0;i<=k;i++){
-8            vector<int>temp=res;// imprtant stepppppp
+8           temp=res;
 9            for(int j=0;j<flights.size();j++){
-10              int source=flights[j][0];
-11              int dest=flights[j][1];
-12              int wt=flights[j][2];
-13              if(res[source] != 1e8 && temp[dest] > res[source]+wt){  // this step is crucial jaha pe hum res mai rewrite hone se bacha re hai using temp vector    /// important step hai 
-14
-15                temp[dest]=res[source]+wt;
-16              }
-17            }
-18              res=temp;
-19        }
-20      
-21        if(res[dst]==1e8)return -1;
-22        return res[dst];
-23    }
-24};
+10                int src=flights[j][0];
+11                int dest=flights[j][1];
+12                int wt=flights[j][2];
+13                if(res[src]!=1e9 && temp[dest]>res[src]+wt){
+14                    temp[dest]=res[src]+wt;
+15                }
+16            }
+17            res=temp;
+18        }
+19        if(res[dst]==1e9)return -1;
+20        return res[dst];
+21    }
+22};
