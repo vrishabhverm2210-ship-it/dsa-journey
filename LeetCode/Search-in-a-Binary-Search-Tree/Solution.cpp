4778a6b18d2ed9d8c4eea65bbe1f1d786af3c19c
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-11 06:53:35 +0530

    Add solution for Search in a Binary Search Tree

--- LeetCode/Search-in-a-Binary-Search-Tree/Solution.cpp
+++ LeetCode/Search-in-a-Binary-Search-Tree/Solution.cpp
@@ -1,16 +1,31 @@
-[5]
-5
-[5]
-7
-[4,2,7,1,3]
-4
-[4,2,7,1,3]
-1
-[4,2,7,1,3]
-6
-[1,null,2,null,3,null,4,null,5]
-5
-[5,4,null,3,null,2,null,1]
-3
-[15,7,20,3,9,17,25,1,5]
-30
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14  TreeNode* fun1(TreeNode* root, int val){
+15    // base condition
+16    if(root==NULL)return NULL;
+17
+18    // three condition bnegi
+19    if(root->val==val){
+20        return root;
+21    }
+22    else if(root->val>val){
+23        return fun1(root->left,val);
+24    }
+25    return fun1(root->right,val);
+26  }
+27
+28    TreeNode* searchBST(TreeNode* root, int val) {
+29        return fun1(root,val);
+30    }
+31};
