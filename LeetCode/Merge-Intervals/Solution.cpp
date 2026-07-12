523ff4e0195a1996c9e4ea3d42ab4bf0d781672e
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-12 22:29:13 +0530

    Add solution for Merge Intervals

--- LeetCode/Merge-Intervals/Solution.cpp
+++ LeetCode/Merge-Intervals/Solution.cpp
@@ -1,8 +1,30 @@
-[[1,3],[2,6],[8,10],[15,18]]
-[[1,4],[4,5]]
-[[1,1],[2,2],[0,0],[2,3],[1,3],[3,5],[2,3],[3,5]]
-[[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]]
-[[1,3],[2,6],[8,10],[8,9],[9,11],[15,18],[2,4],[16,17]]
-[[1012,1136],[1137,1417],[1015,1020]]
-[[1,3]]
-[[1,4],[2,3]]
+1class Solution {
+2public:
+3    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+4        // visualise it as a vector matrix typoo
+5        // important step is to sort it
+6        sort(intervals.begin(),intervals.end());
+7         vector<vector<int>>res;
+8         int n=intervals.size();
+9         int start1=intervals[0][0];
+10         int end1=intervals[0][1];
+11         for(int i=1;i<n;i++){
+12            int start2=intervals[i][0];
+13            int end2=intervals[i][1];
+14            // write merging  condititon
+15            if(end1>=start2){
+16                start1=start1;
+17                end1=max(end1,end2);
+18                continue;
+19            }
+20            else{
+21                res.push_back({start1,end1});
+22                start1=start2;
+23                end1=end2;
+24            }
+25         } 
+26         // last mai jo bacha hoga use bhi result mai daal do
+27         res.push_back({start1,end1});
+28         return res;
+29    }
+30};
