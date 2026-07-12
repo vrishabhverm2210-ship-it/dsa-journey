1fdef0d9bebc0be7b24400f366f09704183ca741
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-12 06:56:09 +0530

    Add solution for Recover Binary Search Tree

--- LeetCode/Recover-Binary-Search-Tree/Solution.cpp
+++ LeetCode/Recover-Binary-Search-Tree/Solution.cpp
@@ -11,43 +11,45 @@
 11 */
 12class Solution {
 13public:
-14TreeNode* wrong1first=NULL;
-15TreeNode* wrong1second=NULL;
-16TreeNode* wrong2first=NULL;
+14TreeNode*  wrong1first=NULL;
+15TreeNode* wrong2first=NULL;
+16TreeNode* wrong1second=NULL;
 17TreeNode* wrong2second=NULL;
-18int glt=0;
-19void fun1(TreeNode* root ,TreeNode* &prev){
-20    if(root==NULL)return;
-21    fun1(root->left,prev);
-22    if(prev == NULL){
-23        prev=root;
-24    }
-25    else{
-26        if(root->val<=prev->val){
-27           if(glt==0){
-28            wrong1first=prev;
-29            wrong1second=root;
-30            glt++;
-31           }
-32           else{
-33            wrong2first=prev;
-34            wrong2second=root;
-35            glt++;
-36           }
-37        }
-38        prev=root;
-39    }
-40      fun1(root->right,prev);
-41}
-42    void recoverTree(TreeNode* root) {
-43        TreeNode* prev=NULL;
-44        fun1(root,prev);
-45        if(glt==1){
-46        swap(wrong1first->val,wrong1second->val);
-47    }
-48    else{
-49        swap(wrong1first->val,wrong2second->val);
-50    }
-51
-52    }
-53};
+18int count=0;
+19void fun1(TreeNode*  root,TreeNode*  &prev){
+20    // base case
+21    if(root==NULL)return;
+22    fun1(root->left,prev);
+23    if(prev==NULL){
+24        prev=root;
+25    }
+26    else{  // we have to handle mistake in this case itself
+27         if(prev->val>=root->val){
+28            if(count==0){
+29                wrong1first=prev;
+30                wrong1second=root;
+31                count++;
+32            }
+33            else{
+34                wrong2first=prev;
+35                wrong2second=root;
+36                count++;
+37            }
+38         }
+39         prev=root;  // must condition required to maintain the regular flow
+40    }
+41    fun1(root->right,prev);
+42}
+43
+44    void recoverTree(TreeNode* root) {
+45        TreeNode* prev=NULL;
+46        fun1(root,prev);
+47        if(count==1){
+48            // means ek hi wrong pair hai adjacent (normally swap them)
+49            swap(wrong1first->val,wrong1second->val);
+50        }
+51        else{
+52            swap(wrong1first->val,wrong2second->val);
+53        }
+54    }
+55};
