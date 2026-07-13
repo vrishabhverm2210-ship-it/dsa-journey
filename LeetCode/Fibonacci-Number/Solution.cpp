9c35e103c0ad31a4c4965b6cfbaf296e17dd297c
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-13 18:33:31 +0530

    Add solution for Fibonacci Number

--- LeetCode/Fibonacci-Number/Solution.cpp
+++ LeetCode/Fibonacci-Number/Solution.cpp
@@ -1,5 +1,17 @@
-int[] ans = 
-{0,1,1,2,3,5,8,13,21,34,55,89,144,
-233,377,610,987,1597,2584,4181,
-6765,10946,17711,28657,46368,
-75025,121393,196418,317811,514229,832040};
+1class Solution {
+2public: 
+3unordered_map<int,int>mpp1;
+4    int fib(int n) {
+5          if(n==0 )return 0;
+6          if(n==1)return 1;
+7          //check in map
+8          if(mpp1.find(n) != mpp1.end())return mpp1[n];
+9          // otherwise store it for the future use , then return it
+10          int t1=fib(n-1);
+11          int t2=fib(n-2);
+12          int ans=t1+t2;
+13          mpp1[n]=ans;
+14          return ans;
+15
+16    }
+17};
