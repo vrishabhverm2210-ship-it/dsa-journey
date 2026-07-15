df812601df4b5c97632871884ff88d33f998a8c7
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-15 09:18:55 +0530

    Add solution for Lowest Common Ancestor of a Binary Tree

--- LeetCode/Lowest-Common-Ancestor-of-a-Binary-Tree/Solution.cpp
+++ LeetCode/Lowest-Common-Ancestor-of-a-Binary-Tree/Solution.cpp
@@ -9,25 +9,23 @@
 9 */
 10class Solution {
 11public:
-12int fun1(TreeNode* root, TreeNode* p, TreeNode* q ,  TreeNode* & ans){
-13    // Base condtion
-14    if(root==NULL)return 0;
-15    // three conditions
-16     int r1= fun1(root->left,p,q,ans);
-17     int r2=fun1(root->right,p,q,ans);
-18    int score=0;
-19    if((root->val== p->val) || (root->val == q->val)){
-20        score=1;
-21    }
-22    int total=score+r1+r2;
-23    if(total==2 && ans ==NULL){
-24        ans=root;
-25    }
-26    return total;
-27}
-28    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-29        TreeNode* ans=NULL;
-30        fun1(root,p,q,ans);
-31        return ans;
-32    }
-33};
+12int fun1(TreeNode*root, TreeNode* p, TreeNode* q,TreeNode* &ans){
+13    if(root==NULL)return 0;
+14    int score1=fun1(root->left,p,q,ans);
+15    int score2=fun1(root->right,p,q,ans);
+16    int self=0;
+17    if(root->val==p->val || root->val ==q->val){
+18        self=1;
+19    }
+20    int total=score1+score2+self;
+21    if(total==2 && ans==NULL){
+22        ans=root;
+23    }
+24    return total;
+25}
+26    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+27         TreeNode* ans=NULL;
+28         fun1(root,p,q,ans);
+29         return ans;
+30    }
+31};
