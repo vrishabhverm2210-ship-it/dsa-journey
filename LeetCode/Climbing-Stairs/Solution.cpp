96092acb554e0a7eb457c467a2609ab8c6bdbca1
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-16 13:10:52 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.cpp
+++ LeetCode/Climbing-Stairs/Solution.cpp
@@ -1,22 +1,33 @@
 1class Solution {
 2public:
-3unordered_map<int,int>mpp1;
-4int fun1(int i,int n){
-5    //  Base cases
-6    if(i==n)return 1;
-7    if(i>n)return 0;
-8    // check in map
-9    if(mpp1.find(i) != mpp1.end()){
-10        return mpp1[i];
-11    }
-12    // otherwise
-13    int r1=fun1(i+1,n);
-14    int r2=fun1(i+2,n);
-15    int ans=r1+r2;
-16    mpp1[i]=ans;
-17    return ans;
-18}
-19    int climbStairs(int n) {
-20        return fun1(0,n);
-21    }
-22};
+3// unordered_map<int,int>mpp1;
+4// int fun1(int i,int n){
+5//     //  Base cases
+6//     if(i==n)return 1;
+7//     if(i>n)return 0;
+8//     // check in map
+9//     if(mpp1.find(i) != mpp1.end()){
+10//         return mpp1[i];
+11//     }
+12
+13//     // otherwise
+14//     int r1=fun1(i+1,n);
+15//     int r2=fun1(i+2,n);
+16//     int ans=r1+r2;
+17//     mpp1[i]=ans;
+18//     return ans;
+19// }
+20
+21    int climbStairs(int n) {
+22        // return fun1(0,n);
+23        // let's solve thsi using tabulation
+24        // make vector dp
+25        vector<int>dp(n+1);
+26        dp[n]=1;
+27        dp[n-1]=1;
+28        for(int i=n-2;i>=0;i--){
+29            dp[i]=dp[i+1]+dp[i+2];
+30        }
+31    return dp[0];
+32    }
+33};
