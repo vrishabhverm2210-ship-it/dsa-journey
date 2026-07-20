44bc4c634de93e5dff40c46eacac77572d4cf953
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-20 08:10:07 +0530

    Add solution for Best Time to Buy and Sell Stock

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
@@ -1,21 +1,21 @@
 1class Solution {
 2public:
-3int fun1(vector<int>& prices,int n, int idx,int k,vector<vector<int>>&dp){
+3int fun1(vector<int>& prices,int n,int idx,int k,  vector<vector<int>>&dp){
 4    if(idx==n)return 0;
 5    if(k==0)return 0;
 6    if(dp[idx][k]!=-1)return dp[idx][k];
-7    if(k==2){   //  buying opportunity hai
-8    int c1=fun1(prices,n,idx+1,k-1,dp)-prices[idx];
-9    int c2=fun1(prices,n,idx+1,k,dp);
-10    return dp[idx][k]= max(c1,c2);
-11    }
-12    else{
-13        int d1=fun1(prices,n,idx+1,k-1,dp)+prices[idx];
-14          int d2=fun1(prices,n,idx+1,k,dp);
-15          return  dp[idx][k]=max(d1,d2);
-16    }
-17    
-18}
+7    if(k==2){
+8        // we have to choices whether to buy or not
+9        int c1=fun1(prices,n,idx+1,k-1,dp)-prices[idx];
+10        int c2=fun1(prices,n,idx+1,k,dp);
+11        return dp[idx][k]=max(c1,c2);
+12    }
+13    // we have two choices for the selling either to sell or notu
+14    int d1=fun1(prices,n,idx+1,k-1,dp)+prices[idx];
+15        int d2=fun1(prices,n,idx+1,k,dp);
+16        return  dp[idx][k]=max(d1,d2);
+17}
+18
 19    int maxProfit(vector<int>& prices) {
 20        int n=prices.size();
 21        int k=2;
@@ -24,6 +24,6 @@
 24            vector<int>t(k+1,-1);
 25            dp[i]=t;
 26        }
-27      return fun1(prices,n,0,2,dp);
+27        return fun1(prices,n,0,k,dp);
 28    }
 29};
