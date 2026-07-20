7e16703994e3a91521d80c0f1e8b81b0651dfe22
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-20 08:41:31 +0530

    Add solution for Best Time to Buy and Sell Stock

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
@@ -17,13 +17,42 @@
 17}
 18
 19    int maxProfit(vector<int>& prices) {
-20        int n=prices.size();
-21        int k=2;
-22        vector<vector<int>>dp(n+1);
-23        for(int i=0;i<=n;i++){
-24            vector<int>t(k+1,-1);
-25            dp[i]=t;
-26        }
-27        return fun1(prices,n,0,k,dp);
-28    }
-29};
+20        // int n=prices.size();
+21        // int k=2;
+22        // vector<vector<int>>dp(n+1);
+23        // for(int i=0;i<=n;i++){
+24        //     vector<int>t(k+1,-1);
+25        //     dp[i]=t;
+26        // }
+27        // return fun1(prices,n,0,k,dp);
+28
+29        // let's try to convert it into tabulation
+30          int n=prices.size();
+31           int k=2;
+32        vector<vector<int>>dp(n+1);
+33        for(int i=0;i<=n;i++){
+34            vector<int>t(k+1,-1);
+35            dp[i]=t;
+36        }
+37        // fill the 0th column
+38        for(int i=0;i<=n;i++){
+39            dp[i][0]=0;
+40        }
+41        // fill the nth row
+42        for(int i=0;i<=2;i++){
+43            dp[n][i]=0;
+44        }
+45        // now run a normal loop
+46        for(int i=n-1;i>=0;i--){
+47            for(int j=1;j<=k;j++){
+48                if(j==2){
+49                dp[i][j]=max(dp[i+1][j-1]-prices[i],dp[i+1][j]);
+50                }
+51                else{
+52                    dp[i][j]=max(dp[i+1][j-1]+prices[i],dp[i+1][j]);
+53                }     
+54            }
+55        }
+56return dp[0][k];
+57    }
+58};
