5fe7522e02848a43145d1e4e07f5418d5ea5cfa2
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-21 06:40:42 +0530

    Add solution for Minimum Size Subarray Sum

--- LeetCode/Minimum-Size-Subarray-Sum/Solution.cpp
+++ LeetCode/Minimum-Size-Subarray-Sum/Solution.cpp
@@ -1,25 +1,22 @@
 1class Solution {
 2public:
 3    int minSubArrayLen(int target, vector<int>& nums) {
-4        int mini=INT_MAX;
-5        int n=nums.size();
-6        int high=0;
+4        int n=nums.size();
+5        int sum=0;
+6        int res=INT_MAX;
 7        int low=0;
-8        int sum=0;
-9        for(high=0;high<n;high++){
-10          sum+=nums[high];
-11          // wrong info block
-12          while(sum>=target){
-13               int len=high-low+1;
-14            mini =min(mini,len);
-15            sum-=nums[low];
-16            low++;
-17        
-18            // int len=high-low+1;
-19            // mini =min(mini,len);
-20          }         
-21        }
-22        if(mini==INT_MAX)return 0;
-23        return mini;
-24    }
-25};
+8        for(int high=0;high<n;high++){
+9            sum+=nums[high];
+10            while(target<=sum){
+11                // first find the length
+12                int len=high-low+1;
+13                res=min(res,len);
+14                // remove the lowth element from the sum
+15                sum-=nums[low];
+16                low++;
+17            }
+18        }
+19        if(res==INT_MAX)return 0;
+20        return res;
+21    }
+22};
