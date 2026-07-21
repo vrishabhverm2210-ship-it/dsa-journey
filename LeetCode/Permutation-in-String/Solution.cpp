888ce90dae333eb54ffd81ba12a61030277a0e16
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-21 07:37:34 +0530

    Add solution for Permutation in String

--- LeetCode/Permutation-in-String/Solution.cpp
+++ LeetCode/Permutation-in-String/Solution.cpp
@@ -1,47 +1,26 @@
 1class Solution {
-2    private:
-3    bool checkequal(int a[26],int b[26]){
-4        for(int i=0;i<26;i++){
-5            if(a[i]!=b[i]){
-6                return 0;
-7            }
-8        }return 1;
-9    }
-10public:
-11    bool checkInclusion(string s1, string s2) {
-12        //character count array from s1
-13   int count1[26]={0};
-14   for(int i=0;i<s1.length();i++){
-15   int index=s1[i]-'a';
-16   count1[index]++;
-17   }
-18     // traverse s2 string in window of size s1 length
-19    int i=0;
-20    int windowsize=s1.length();
-21    int count2[26]={0};
-22    //running for first window
-23    while(i<windowsize&&i<s2.length()){
-24          int index=s2[i]-'a';
-25          count2[index]++;
-26          i++;
-27    }
-28    if (checkequal (count1,count2) )
-29        return 1;
-30
-31    //aage window process krow
-32    while(i<s2.length()){
-33        char newchar=s2[i];
-34        int index=newchar-'a';
-35        count2[index]++;
-36        char oldchar=s2[i-windowsize];
-37          index=oldchar-'a';
-38         count2[index]--;
-39         i++;
-40    
-41     if (checkequal(count1,count2) )
-42        return 1;
-43
-44    }
-45  return 0;   
-46}
-47};
+2public:
+3    bool checkInclusion(string s1, string s2) {
+4        // let's make sliding window of s1.size()
+5        int k=s1.size();
+6        // store the frequency 
+7        unordered_map<char,int>mpp1;
+8        for(int i=0;i<s1.size();i++){
+9            mpp1[s1[i]]++;
+10        }
+11        int low=0;
+12         unordered_map<char,int>mpp2;
+13        for(int high=0;high<s2.size();high++){
+14           mpp2[s2[high]]++;
+15           if(high - low + 1>k){
+16            mpp2[s2[low]]--;
+17            if(mpp2[s2[low]]==0)mpp2.erase(s2[low]);
+18            low++;
+19           }
+20           if(high - low + 1==k){
+21            if(mpp1==mpp2)return true;
+22           }
+23        }
+24        return false;
+25    }
+26};
