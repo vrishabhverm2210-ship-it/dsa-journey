858c36644d3bf8e21ab8f6be4c6d13583bfe2506
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-22 13:02:38 +0530

    Add solution for Peak Index in a Mountain Array

--- LeetCode/Peak-Index-in-a-Mountain-Array/Solution.cpp
+++ LeetCode/Peak-Index-in-a-Mountain-Array/Solution.cpp
@@ -6,16 +6,20 @@
 6        int res=-1;
 7        while(low<=high){
 8            int mid=low+(high-low)/2;
-9            if(arr[mid]<arr[mid+1]){
-10                // we are in the first  half so increase the low
-11                low=mid+1;
-12            }
-13            else{
-14                // we are in the second half try minimising
-15                res=mid;
-16                high=mid-1;
-17            }
-18        }
-19        return res;
-20    }
-21};
+9                  if(mid == arr.size() - 1){
+10        res = mid;
+11        break;
+12    }
+13            if(arr[mid]<arr[mid+1]){     // can do mid+1 cause it gurantees peak
+14                // we are in the first  half so increase the low
+15                low=mid+1;
+16            }
+17            else{
+18                // we are in the second half try minimising
+19                res=mid;
+20                high=mid-1;
+21            }
+22        }
+23        return res;
+24    }
+25};
