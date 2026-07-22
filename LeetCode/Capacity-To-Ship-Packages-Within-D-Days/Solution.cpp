28c3977f7fc358da3262ed9abf815d7846141cc3
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-22 18:05:29 +0530

    Add solution for Capacity To Ship Packages Within D Days

--- LeetCode/Capacity-To-Ship-Packages-Within-D-Days/Solution.cpp
+++ LeetCode/Capacity-To-Ship-Packages-Within-D-Days/Solution.cpp
@@ -1,36 +1,40 @@
 1class Solution {
 2public:
-3bool isPossible(vector<int>& weights, int days,int guess){
-4    int sum=0;
-5    int count=1;
+3bool ispossible(vector<int>& weights, int days, int mid){
+4    int count=1;
+5    int sum=0;
 6    for(int i=0;i<weights.size();i++){
 7        sum+=weights[i];
-8        if(sum>guess){
-9            count++;
-10            sum=weights[i];
+8        if(sum>mid){
+9            sum=weights[i];
+10            count++;
 11            if(count>days)return false;
 12        }
 13    }
-14  return true;
+14    return true;
 15}
 16    int shipWithinDays(vector<int>& weights, int days) {
-17        int res=-1;
-18        int low=INT_MIN;
-19        int high=0;
-20        for(int i=0;i<weights.size();i++){
+17        int high=0;   // sum ho jayega
+18         int low=INT_MIN; // MAXIMUM BECAUSE HUM EK BLOCK KO TOD KE NI DAAL SKTE MIN HUME HIGHEST VALI CAPACITY TOH CHAHIYE HI
+19        for(int i=0; i<weights.size();i++){
+20            high+=weights[i];
 21            low=max(low,weights[i]);
-22            high+=weights[i];
-23        }
-24        while(low<=high){
-25            int mid=low+(high-low)/2;
-26            if(isPossible(weights,days,mid)){
-27                res=mid;
-28                high=mid-1;
-29            }
-30            else{
-31                low=mid+1;
-32            }
-33        }
-34        return res;
-35    }
-36};
+22        }
+23        
+24        int res=-1; 
+25         while(low<=high){
+26            int mid=low+(high-low)/2;
+27            if(ispossible(weights,days,mid)){
+28                // store the answer and search for the more minimum 
+29                res=mid;
+30                high=mid-1;
+31            }
+32            else{
+33                low=mid+1;
+34            }
+35        }
+36        return res;
+37
+38
+39    }
+40};
