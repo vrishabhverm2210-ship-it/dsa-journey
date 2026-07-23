1e1ce877eef6e33645e049908bb28640465cf160
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-23 21:18:02 +0530

    Add solution for Remove All Adjacent Duplicates in String II

--- LeetCode/Remove-All-Adjacent-Duplicates-in-String-II/Solution.cpp
+++ LeetCode/Remove-All-Adjacent-Duplicates-in-String-II/Solution.cpp
@@ -1,35 +1,35 @@
 1class Solution {
 2public:
 3    string removeDuplicates(string s, int k) {
-4        stack<pair<char,int>>st;
-5        string res="";
-6        int n=s.size();
-7        for(int i=0;i<n;i++){
-8            if(st.empty()){
-9                st.push({s[i],1});
-10                continue;
-11            }
-12            if(st.top().first != s[i]){
-13                st.push({s[i],1});
-14                continue;
-15            }
-16            if(st.top().first==s[i] && st.top().second<k-1){
-17                pair<char,int>top=st.top();
-18                st.pop();
-19                st.push({top.first,top.second+1});
-20                continue;
-21            }
-22            else{
-23                st.pop();   // puri ki puri entry hi udegi
-24            }
-25        }
-26        while(!st.empty()){
-27            pair<char,int>top=st.top();
+4        int n=s.size();
+5        stack<pair<char,int>>st;
+6       for(int i=0;i<n;i++){
+7        // first case that if hmara stack is empty
+8        if(st.empty()){
+9            st.push({s[i],1});
+10        }
+11        else if(st.top().first!=s[i]){
+12            st.push({s[i],1});
+13        }
+14        else if(st.top().first==s[i] && st.top().second<k-1){
+15            // in this case only increase the frequency
+16            pair<char,int>p=st.top();
+17            st.pop();
+18            st.push({p.first,p.second+1});
+19        }
+20        else{
+21            st.pop();
+22        }
+23       } 
+24         // now extract the answer from the stack and push it into the res vector
+25         string res="";
+26         while(!st.empty()){
+27            pair<char,int>p=st.top();
 28            st.pop();
-29            while(top.second--){
-30                res.push_back(top.first);
+29            for(int i=0;i<p.second;i++){
+30            res.push_back(p.first);
 31            }
-32        }
+32         }
 33        reverse(res.begin(),res.end());
 34        return res;
 35    }
