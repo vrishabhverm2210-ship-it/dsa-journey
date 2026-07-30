c48ed18285facad1c6519ff602736c336dfa1ae4
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-07-30 18:35:20 +0530

    Add solution for Construct Binary Tree from Preorder and Inorder Traversal

--- LeetCode/Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/Solution.cpp
+++ LeetCode/Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/Solution.cpp
@@ -10,31 +10,26 @@
 10 * };
 11 */
 12class Solution {
-13public:
-14int idx=0;
-15TreeNode* fun1(vector<int>& preorder, unordered_map<int,int> &mpp1,int low,int high){
-16    // make a base case
-17    if(high<low)return NULL;
-18    // make a node (SYNTAX)
-19    TreeNode* node = new TreeNode(preorder[idx]);  // for now it's left and right child is null
-20    idx++;
-21    // now search this node from the inorder map
-22    int id=mpp1[node->val];  // we got our id from this step
-23    // make to call to decide that which part should be proceeseed in right and which to left
-24    node->left=fun1(preorder,mpp1,low,id-1);
-25    node->right=fun1(preorder,mpp1,id+1,high);
-26    return node;
-27}
-28    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-29        // store inorder traversal in the map
-30        unordered_map<int,int>mpp1;
-31        for(int i=0;i<inorder.size();i++){
-32            mpp1[inorder[i]]=i;
-33        }
-34        // once store so let's call the function(in which we are traversing the preorder array step by step)
-35        int low=0;
-36        int high=preorder.size()-1;
-37         return  fun1(preorder,mpp1,low,high);
-38
-39    }
-40};
+13public:  
+14int idx=0; // using this we will traverse the preorder vector
+15TreeNode* fun1(vector<int>& preorder ,  unordered_map<int,int>&mpp1 ,int low, int high){
+16    // base case
+17    if(low>high)return NULL;
+18 // create the node
+19
+20    TreeNode* node = new TreeNode(preorder[idx]);
+21    idx++;
+22    int id=mpp1[node->val];
+23    node->left=fun1(preorder,mpp1,low,id-1);
+24    node->right=fun1(preorder,mpp1,id+1,high);
+25    return node;
+26}
+27    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+28      // firstly add all the elements of the inorder array into the mappp with it's index
+29      unordered_map<int,int>mpp1;
+30      for(int i=0;i<inorder.size();i++){
+31        mpp1[inorder[i]]=i;
+32      } 
+33      return fun1(preorder,mpp1,0,preorder.size()-1);
+34    }
+35};
