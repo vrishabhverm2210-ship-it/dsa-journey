b9c605eec79df85a36e8272117219a7b964f4386
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-03 06:39:56 +0530

    Add solution for Maximum Subarray

--- LeetCode/Maximum-Subarray/Solution.cpp
+++ LeetCode/Maximum-Subarray/Solution.cpp
@@ -1,14 +1,15 @@
 1class Solution {
 2public:
 3    int maxSubArray(vector<int>& nums) {
-4        int bestending=nums[0];
-5        int ans=nums[0];
-6        for(int i=1;i<nums.size();i++){
-7            int choice1=bestending+nums[i]; // continue journey
-8            int choice2=nums[i];            // start new
-9         bestending= max(choice1,choice2); 
-10         ans = max(ans,bestending);
-11        }
-12        return ans;
-13    }
-14};
+4        int n=nums.size();
+5        int res=nums[0];
+6        int bestpossible=nums[0];
+7        for(int i=1;i<n;i++){
+8            int choice1= nums[i];   // start a new journey ignorring your past
+9            int choice2=bestpossible+nums[i];  // continue journey along with ur past
+10            bestpossible =max(choice1,choice2);
+11            res=max(res,bestpossible);
+12        }
+13        return res;
+14    }
+15};
