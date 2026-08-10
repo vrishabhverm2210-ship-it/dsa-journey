a4b312692c84d561820348552a9f6f6a674cea3f
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-10 05:55:54 +0530

    Add solution for Longest Common Subsequence

--- LeetCode/Longest-Common-Subsequence/Solution.cpp
+++ LeetCode/Longest-Common-Subsequence/Solution.cpp
@@ -1,46 +1,31 @@
 1class Solution {
 2public:
-3// int fun1(string text1, string text2,int i,int j){
-4//     if(i==text1.size() || j== text2.size() ){
-5//         return 0;
-6//     }
-7//     // we have 1 case when both are equal
-8//     if(text1[i]==text2[j]){
-9//         return 1+fun1(text1,text2,i+1,j+1);
-10//     }
-11//     // now we have two choices kisi ek ko toh aage bhadna hi pdega
-12//     int c1=fun1(text1,text2,i+1,j);
-13//     int c2=fun1(text1,text2,i,j+1);
-14//     return max(c1,c2);
-15// }
-16    int longestCommonSubsequence(string text1, string text2) {
-17        // return fun1(text1,text2,0,0);
-18        // make 2d vector
-19        int n=text1.size();
-20        int m=text2.size();
-21        vector<vector<int>>dp(n+1);
-22        for(int i=0;i<=n;i++){
-23            vector<int>t(m+1,-1);
-24            dp[i]=t;
-25        }
-26        // now according to base case fill the matrix
-27        for(int i=0;i<=m;i++){
-28            dp[n][i]=0;
-29        }
-30         for(int i=0;i<=n;i++){
-31            dp[i][m]=0;
-32        }
-33        // now normal loops ulti chlegi
-34        for(int i=n-1;i>=0;i--){
-35            for(int j=m-1;j>=0;j--){
-36                if(text1[i]==text2[j]){
-37                    dp[i][j]=1+dp[i+1][j+1];
-38                }
-39                else{
-40                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
-41                }
-42            }
-43        }
-44return dp[0][0];
-45    }
-46};
+3int fun1(string &text1, string &text2,int  n ,int m,vector<vector<int>> &dp){
+4    if(n==0 || m==0){
+5        return 0;
+6    }
+7    if(dp[n][m]!=-1)return dp[n][m];
+8    if(text1[n-1]==text2[m-1]){
+9        return  dp[n][m]=1+fun1(text1,text2,n-1,m-1,dp);
+10    }
+11    // we have two choices
+12
+13    int c1=fun1(text1,text2,n-1,m,dp);
+14    int c2=fun1(text1,text2,n,m-1,dp);
+15    return dp[n][m]= max(c1,c2);
+16}
+17    int longestCommonSubsequence(string text1, string text2) {
+18        // recursive code ->think of a smlestvalid input
+19        // choice diagram 
+20        // how we can decrease our size
+21        // for memoization let's make the dp array
+22        int n=text1.size();
+23          int m=text2.size();      
+24        vector<vector<int>>dp(n+1);
+25        for(int i=0;i<=n;i++){
+26             vector<int>t(m+1,-1);
+27             dp[i]=t;
+28        }
+29        return fun1(text1,text2,text1.size(),text2.size(),dp);
+30    }
+31};
