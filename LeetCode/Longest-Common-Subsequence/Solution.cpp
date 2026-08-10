657d51a58dc3aa9cc3cc10c33b3e63376ded15a7
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-10 06:06:52 +0530

    Add solution for Longest Common Subsequence

--- LeetCode/Longest-Common-Subsequence/Solution.cpp
+++ LeetCode/Longest-Common-Subsequence/Solution.cpp
@@ -21,11 +21,36 @@
 21        // for memoization let's make the dp array
 22        int n=text1.size();
 23          int m=text2.size();      
-24        vector<vector<int>>dp(n+1);
-25        for(int i=0;i<=n;i++){
-26             vector<int>t(m+1,-1);
-27             dp[i]=t;
-28        }
-29        return fun1(text1,text2,text1.size(),text2.size(),dp);
-30    }
-31};
+24        // vector<vector<int>>dp(n+1);
+25        // for(int i=0;i<=n;i++){
+26        //      vector<int>t(m+1,-1);
+27        //      dp[i]=t;
+28        // }
+29        // return fun1(text1,text2,text1.size(),text2.size(),dp);
+30        // now let's do it's bottom up
+31          vector<vector<int>>dp(n+1);
+32        for(int i=0;i<=n;i++){
+33             vector<int>t(m+1,-1);
+34             dp[i]=t;
+35        }
+36        // initialise according to the base case
+37        for(int i=0;i<=n;i++){
+38            dp[i][0]=0;
+39        }
+40        for(int i=0;i<=m;i++){
+41            dp[0][i]=0;
+42        }
+43        // now fill the remaining pllaces of matrix using the normal loops
+44        for(int i=1;i<=n;i++){
+45            for(int j=1;j<=m;j++){
+46                if(text1[i-1]==text2[j-1]){
+47                    dp[i][j]=1+dp[i-1][j-1];
+48                }
+49                else{
+50                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+51                }
+52            }
+53        }
+54        return dp[n][m];
+55    }
+56};
