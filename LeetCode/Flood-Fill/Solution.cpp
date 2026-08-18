ace7b0a3b45a016f6820d01e46bf4e407d883983
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-18 08:04:05 +0530

    Add solution for Flood Fill

--- LeetCode/Flood-Fill/Solution.cpp
+++ LeetCode/Flood-Fill/Solution.cpp
@@ -17,25 +17,26 @@
 17      
 18        queue<pair<int,pair<int,int>>>q;
 19        q.push({element,{sr,sc}});
-20        while(!q.empty()){
-21           pair<int,pair<int,int>>p=q.front();
-22           q.pop();
-23           int c=p.first;
-24           int i=p.second.first;
-25           int j=p.second.second;
-26          res[i][j]=color;
-27          for(int k=0;k<4;k++){
-28            int row=i+x[k];
-29            int col=j+y[k];
-30            if(isvalid(row,col,n,m) && image[row][col]==element){
-31            
-32                q.push({element,{row,col}});
-33                image[row][col]=-1;
-34            }
-35          }
-36
+20        image[sr][sc]=-1;
+21        while(!q.empty()){
+22           pair<int,pair<int,int>>p=q.front();
+23           q.pop();
+24           int c=p.first;
+25           int i=p.second.first;
+26           int j=p.second.second;
+27          res[i][j]=color;
+28          for(int k=0;k<4;k++){
+29            int row=i+x[k];
+30            int col=j+y[k];
+31            if(isvalid(row,col,n,m) && image[row][col]==element){
+32            
+33                q.push({element,{row,col}});
+34                image[row][col]=-1;
+35            }
+36          }
 37
-38        }
-39        return res;
-40            }
-41};
+38
+39        }
+40        return res;
+41            }
+42};
