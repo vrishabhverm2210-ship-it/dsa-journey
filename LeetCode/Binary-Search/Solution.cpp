3dbc3702972579c8a831bd839101fb4b43a8c14f
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-25 05:11:48 +0530

    Add solution for Binary Search

--- LeetCode/Binary-Search/Solution.cpp
+++ LeetCode/Binary-Search/Solution.cpp
@@ -1,16 +1,21 @@
 1class Solution {
 2public:
 3    int search(vector<int>& nums, int target) {
-4        int low=0;
-5        int high=nums.size()-1;
-6        while(high>=low){
-7            int mid= (low+high)/2;
-8            if(nums[mid]==target)return mid;
-9            else if(nums[mid]<target)low=mid+1;
-10            else{
-11                high=mid-1;
-12            }
-13        }
-14        return -1;
-15    }
-16};
+4        int n=nums.size();
+5        int low=0;
+6        int high=n-1;
+7        while(high>=low){
+8            int mid=low+(high-low)/2;
+9            if(nums[mid]==target)return mid;
+10            else if(nums[mid]>target)
+11            {
+12                high=mid-1;
+13            }
+14            else
+15            {
+16                low=mid+1;
+17            }
+18        }
+19        return -1;
+20    }
+21};
