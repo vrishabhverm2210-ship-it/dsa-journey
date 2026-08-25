6788b36276db996c09eb16647c9137c3158aae6e
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-25 05:43:29 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.cpp
+++ LeetCode/Valid-Parentheses/Solution.cpp
@@ -1,24 +1,25 @@
 1class Solution {
 2public:
 3    bool isValid(string s) {
-4        int n=s.size();
-5        stack<char>st;
-6        for(int i=0;i<n;i++){
-7            char ch=s[i];
-8            if(ch=='{' || ch=='('|| ch == '['){
-9                st.push(ch);
-10            }
-11            else{   // ofcourse it's closing
-12                if(st.empty())return false;
-13                if(st.top()=='[' && ch==']' || st.top()=='{' && ch=='}'|| st.top()=='(' && ch==')' ){
-14                    st.pop(); 
-15                }
-16                else{
-17                    return false;
-18                }
-19            }
-20        }
-21        if(!st.empty())return false;
-22        return true;
-23    }
-24};
+4        stack<char>st;
+5        for(int i=0;i<s.size();i++){
+6            char ch=s[i];
+7            if(ch=='{' || ch=='(' || ch=='['){
+8                st.push(ch);
+9            }
+10            else {  // means closed bracket
+11            // firstly check is there any open bracket if not return straighway flse;
+12            if(st.empty())return false;
+13            if((st.top()=='[' && ch==']' )||(st.top()=='(' && ch==')' ) || (st.top()=='{' && ch=='}' ) ){
+14                st.pop();
+15            }
+16            else {
+17                return false;
+18            }
+19
+20            }
+21        }
+22        if(!st.empty())return false;
+23        return true;
+24    }
+25};
