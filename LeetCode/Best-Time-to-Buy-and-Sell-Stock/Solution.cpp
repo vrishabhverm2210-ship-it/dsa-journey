144f639ba0d7653469b06495cb51be2bf3f034b0
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-26 08:26:55 +0530

    Add solution for Best Time to Buy and Sell Stock

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
@@ -1,32 +1,58 @@
 1class Solution {
 2public:
-3int  fun1(vector<int>& prices,int k,int idx,vector<vector<int>>&dp){
-4    if(idx==prices.size()){
-5        return 0 ;
-6    }
-7    if(k==0)return 0;
-8   if(dp[idx][k]!=-1)return dp[idx][k];
-9    if(k==2){
-10        // we have choice to buy
-11        int c1=fun1(prices,k-1,idx+1,dp)-prices[idx];
-12        int c2=fun1(prices,k,idx+1,dp);
-13        return dp[idx][k]=max(c1,c2);
-14    }
-15  // we have choice to buy 
-16        int c1=fun1(prices,k-1,idx+1,dp)+prices[idx];
-17        int c2=fun1(prices,k,idx+1,dp);
-18        return dp[idx][k]=max(c1,c2);
-19    
-20
-21}
-22    int maxProfit(vector<int>& prices) {
-23        int k=2;
-24         vector<vector<int>>dp(prices.size()+1);
-25         for(int i=0;i<=prices.size();i++){
-26            vector<int>t(k+1,-1);
-27            dp[i]=t;
-28         }
-29        return  fun1(prices, k,0,dp);
-30      
-31    }
-32};
+3int fun1(vector<int>& prices,int n,int idx,int k,  vector<vector<int>>&dp){
+4    if(idx==n)return 0;
+5    if(k==0)return 0;
+6    if(dp[idx][k]!=-1)return dp[idx][k];
+7    if(k==2){
+8        // we have to choices whether to buy or not
+9        int c1=fun1(prices,n,idx+1,k-1,dp)-prices[idx];
+10        int c2=fun1(prices,n,idx+1,k,dp);
+11        return dp[idx][k]=max(c1,c2);
+12    }
+13    // we have two choices for the selling either to sell or notu
+14    int d1=fun1(prices,n,idx+1,k-1,dp)+prices[idx];
+15        int d2=fun1(prices,n,idx+1,k,dp);
+16        return  dp[idx][k]=max(d1,d2);
+17}
+18
+19    int maxProfit(vector<int>& prices) {
+20        // int n=prices.size();
+21        // int k=2;
+22        // vector<vector<int>>dp(n+1);
+23        // for(int i=0;i<=n;i++){
+24        //     vector<int>t(k+1,-1);
+25        //     dp[i]=t;
+26        // }
+27        // return fun1(prices,n,0,k,dp);
+28
+29        // let's try to convert it into tabulation
+30          int n=prices.size();
+31           int k=2;
+32        vector<vector<int>>dp(n+1);
+33        for(int i=0;i<=n;i++){
+34            vector<int>t(k+1,-1);
+35            dp[i]=t;
+36        }
+37        // fill the 0th column
+38        for(int i=0;i<=n;i++){
+39            dp[i][0]=0;
+40        }
+41        // fill the nth row
+42        for(int i=0;i<=2;i++){
+43            dp[n][i]=0;
+44        }
+45        // now run a normal loop
+46        for(int i=n-1;i>=0;i--){
+47            for(int j=1;j<=k;j++){
+48                if(j==2){
+49                dp[i][j]=max(dp[i+1][j-1]-prices[i],dp[i+1][j]);
+50                }
+51                else{
+52                    dp[i][j]=max(dp[i+1][j-1]+prices[i],dp[i+1][j]);
+53                }     
+54            }
+55        }
+56return dp[0][k];
+57    }
+58};
