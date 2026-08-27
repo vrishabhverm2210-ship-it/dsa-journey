9997606018dfb11bda49ff80618f0e0698bde777
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-27 08:07:55 +0530

    Add solution for Max Area of Island

--- LeetCode/Max-Area-of-Island/Solution.cpp
+++ LeetCode/Max-Area-of-Island/Solution.cpp
@@ -2,41 +2,40 @@
 2public:
 3int x[4]={-1,1,0,0};
 4int y[4]={0,0,-1,1};
-5
-6bool isvalid(int row,int col,int n,int m ){
-7    if(row >=0 && row<n && col >=0 && col<m){
-8        return true;
-9    }
-10    return false;
-11}
-12void dfs(vector<vector<int>>& grid , int i,int j,int &length){
-13    grid[i][j]=2;
-14    int n=grid.size();
-15    int m=grid[0].size();
-16    for(int k=0;k<4;k++){
-17        int row=i+x[k];
-18        int col=j+y[k];
-19      
-20        if(isvalid(row,col,n,m) && grid[row][col]==1){
-21            length++;
-22            dfs(grid,row,col,length);
-23            
-24        }
-25    }
-26}
-27    int maxAreaOfIsland(vector<vector<int>>& grid) {
-28            int maxi=INT_MIN;
-29            int length=1;
-30        for(int i=0;i<grid.size();i++){
-31            for(int j=0;j<grid[0].size();j++){
+5bool isvalid(int row,int col,int n,int m){
+6    if(row<0 || row>=n || col<0 || col>=m){
+7        return false;
+8    }
+9    return true;
+10}
+11void dfs(vector<vector<int>>& grid,int i,int j,int &temp){
+12      int n=grid.size();
+13        int m=grid[0].size();
+14        grid[i][j]=-1;
+15       for(int k=0;k<4;k++){
+16        int row=i+x[k];
+17        int col=j+y[k];
+18        if(isvalid(row,col,n,m) && grid[row][col]==1){
+19            temp+=1;
+20            dfs(grid,row,col,temp);
+21        }
+22    }
+23}
+24    int maxAreaOfIsland(vector<vector<int>>& grid) {
+25        int maxi=INT_MIN;
+26        int n=grid.size();
+27        int m=grid[0].size();
+28        
+29        for(int i=0;i<n;i++){
+30            for(int j=0;j<m;j++){
+31                     int temp=1;
 32                if(grid[i][j]==1){
-33                    length=1;
-34                    dfs(grid,i,j,length);
-35                    maxi=max(maxi,length);
-36                }
-37            }
-38        }
-39         if(maxi==INT_MIN)return 0;
-40         return maxi;
-41    }
-42};
+33                     dfs(grid,i,j,temp);
+34                     maxi=max(maxi,temp);
+35                }
+36            }
+37        }
+38        if(maxi==INT_MIN)return 0;
+39        return maxi;
+40    }
+41};
