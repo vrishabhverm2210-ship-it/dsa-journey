99540ec9c030ead4ab2e955359bbebc9cc16e7d5
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-08-28 06:50:41 +0530

    Add solution for Flood Fill

--- LeetCode/Flood-Fill/Solution.cpp
+++ LeetCode/Flood-Fill/Solution.cpp
@@ -2,41 +2,29 @@
 2public:
 3int x[4]={-1,1,0,0};
 4int y[4]={0,0,-1,1};
-5
-6bool isvalid(int row,int col,int n,int m ){
-7    if(row >=0 && row<n && col >=0 && col<m){
-8        return true;
-9    }
-10    return false;
-11}
-12    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-13        int element=image[sr][sc];
-14        int n=image.size();
-15        int m=image[0].size();
-16        vector<vector<int>>res=image;
-17      
-18        queue<pair<int,pair<int,int>>>q;
-19        q.push({element,{sr,sc}});
-20        image[sr][sc]=-1;
-21        while(!q.empty()){
-22           pair<int,pair<int,int>>p=q.front();
-23           q.pop();
-24           int c=p.first;
-25           int i=p.second.first;
-26           int j=p.second.second;
-27          res[i][j]=color;
-28          for(int k=0;k<4;k++){
-29            int row=i+x[k];
-30            int col=j+y[k];
-31            if(isvalid(row,col,n,m) && image[row][col]==element){
-32            
-33                q.push({element,{row,col}});
-34                image[row][col]=-1;
-35            }
-36          }
-37
-38
-39        }
-40        return res;
-41            }
-42};
+5bool isvalid(int row,int col,int n,int m){
+6    if(row<0 || row>=n || col<0 || col>=m)return false;
+7    return true;
+8}
+9void dfs(vector<vector<int>>& image,int sr,int sc,int color,int c){
+10    int n=image.size();
+11    int m=image[0].size();
+12    image[sr][sc]=color;
+13    for(int k=0;k<4;k++){
+14        int row=sr+x[k];
+15        int col=sc+y[k];
+16        if(isvalid(row,col,n,m) && image[row][col]==c){
+17            dfs(image,row,col,color,c);
+18        }
+19    }
+20}
+21    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+22        // there is no need to run a loop like the no of island questions
+23        // we can directly start dfs from image[sr][sc]
+24        int c=image[sr][sc];
+25        if(c == color)
+26    return image;
+27        dfs(image,sr,sc,color,c);
+28        return image;
+29    }
+30};
