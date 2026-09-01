4866774bb9b0a7e476c3051f0e685df99d40d77d
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-01 17:45:34 +0530

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.cpp
+++ LeetCode/Top-K-Frequent-Elements/Solution.cpp
@@ -1,33 +1,30 @@
 1class Solution {
 2public:
-3struct emp{
-4    bool operator()(pair<int,int>&a,pair<int,int>&b){
-5        if(a.first!=b.first){
-6            return a.first>b.first;
+3    vector<int> topKFrequent(vector<int>& nums, int k) {
+4        unordered_map<int,int>mpp1;  // to store the element of array and their frequency
+5        for(int i=0;i<nums.size();i++){
+6            mpp1[nums[i]]++;
 7        }
-8        return a.second>b.second;
-9    }
-10};
-11    vector<int> topKFrequent(vector<int>& nums, int k) {
-12        unordered_map<int,int>mpp1;
-13        for(int i=0;i<nums.size();i++){
-14            mpp1[nums[i]]++;
-15        }
-16        // create the min heap
-17        priority_queue<pair<int,int>,vector<pair<int,int>>,emp>pq;
-18        for(auto i:mpp1){
-19            pair<int,int>curr={i.second,i.first};
-20            pq.push(curr);
-21            if(pq.size()>k){
-22                pq.pop();
-23            }
-24        }
+8        // now construct the min heap
+9        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+10        for(auto i:mpp1){
+11            int el=i.first;
+12            int freq=i.second;
+13            if(pq.size()<k){
+14          pq.push({freq,el});
+15          continue;
+16            }
+17            pq.push({freq,el});
+18            pq.pop();
+19        }
+20vector<int>res;
+21while(!pq.empty()){
+22    pair<int,int>curr=pq.top();
+23    pq.pop();
+24    res.push_back(curr.second);
 25
-26        vector<int> res;
-27        while(!pq.empty()){
-28            res.push_back(pq.top().second);
-29            pq.pop();
-30        }
-31return res;
-32    }
-33};
+26}
+27reverse(res.begin(),res.end());
+28return res;
+29    }
+30};
