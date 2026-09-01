08728a12d78af66cc7249695eacc58b1b5907450
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-01 08:16:34 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
@@ -4,40 +4,37 @@
 4        int n=nums.size();
 5        int low=0;
 6        int high=n-1;
-7        while(low<=high){
+7    while(low<=high){
 8            int mid=low+(high-low)/2;
 9            if(nums[mid]==target)return mid;
-10            // now firstly determine the half ,whether our middle belongs to first half or second
-11            else if(nums[mid] > nums[n-1]){
-12                // first half
-13                if(target>nums[mid]){
-14                    low=mid+1;
-15                }
-16                else{
-17                    // there are two possible condition
-18                    if(target >nums[n-1]){
-19                        high=mid-1;
-20                    }
-21                    else{
-22                        low=mid+1;
-23                    }
-24                }
-25            }
-26            else{
-27                if(target<nums[mid]){
-28                    high=mid-1;
-29                }
-30                else{
-31                    //  // there are two possible condition
-32                    if(target>nums[n-1]){
-33                        high=mid-1;
+10            if(nums[mid]<=nums[n-1]){
+11                // first half
+12                if(nums[mid]<target){
+13                    if(target>nums[n-1]){
+14                        high=mid-1;
+15                    }
+16                    else{
+17                        low=mid+1;
+18                    }
+19                }
+20                else{
+21                    high=mid-1;
+22                }
+23            }
+24            else{//second half
+25                if(nums[mid]<target){
+26                    low=mid+1;
+27                }
+28                else{
+29                    if(target>nums[n-1]){
+30                        high=mid-1;
+31                    }
+32                    else{
+33                        low=mid+1;
 34                    }
-35                    else{
-36                        low=mid+1;
-37                    }
-38                }
-39            }
-40        }
-41        return -1;
-42    }
-43};
+35                }
+36            }
+37        }
+38        return -1;
+39    }
+40};
