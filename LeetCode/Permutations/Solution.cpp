46392e0d49587cba959aecd2a8b8165b926b722d
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-04 12:17:48 +0530

    Add solution for Permutations

---
+++ LeetCode/Permutations/Solution.cpp
@@ -0,0 +1,23 @@
+1class Solution {
+2public:
+3void fun1(vector<int>& nums,int idx,vector<int>&temp,vector<vector<int>>& res){
+4    // base condition
+5    if(idx==nums.size()){
+6        res.push_back(nums);
+7        return ;
+8    }
+9    for(int i=idx;i<nums.size();i++){
+10        swap(nums[i],nums[idx]);
+11        fun1(nums,idx+1,temp,res);
+12        swap(nums[i],nums[idx]);
+13    }
+14return;
+15
+16}
+17    vector<vector<int>> permute(vector<int>& nums) {
+18        vector<vector<int>> res;
+19        vector<int>temp;
+20        fun1(nums,0,temp,res);
+21        return res;
+22    }
+23};
