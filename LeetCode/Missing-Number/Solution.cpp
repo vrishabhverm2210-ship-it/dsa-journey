d151497232750b1e8cf2c1574eba9f1d8503a496
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-10 06:53:07 +0530

    Add solution for Missing Number

--- LeetCode/Missing-Number/Solution.cpp
+++ LeetCode/Missing-Number/Solution.cpp
@@ -1,17 +1,13 @@
 1class Solution {
 2public:
 3    int missingNumber(vector<int>& nums) {
-4        unordered_map<int,int>mpp1;
-5        for(int i=0;i<nums.size();i++){
-6            mpp1[nums[i]]=1;
-7        }
-8        int n=nums.size();
-9        for(int i=0;i<=n;i++){
-10            if(mpp1.find(i)!=mpp1.end())continue;
-11            else{
-12                return i;
-13            }
-14        }
-15        return -1;
-16    }
-17};
+4        // let's try to build the XOR solution
+5        int n=nums.size();
+6        int ans=n;
+7        for(int i=0;i<n;i++){
+8        ans=ans^i;
+9        ans=ans^nums[i];
+10        }
+11        return ans;
+12    }
+13};
