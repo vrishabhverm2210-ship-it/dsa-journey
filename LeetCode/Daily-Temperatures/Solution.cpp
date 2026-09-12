369c20577d90e43e03c8e58dd5173eac891c3a53
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-12 08:39:30 +0530

    Add solution for Daily Temperatures

--- LeetCode/Daily-Temperatures/Solution.cpp
+++ LeetCode/Daily-Temperatures/Solution.cpp
@@ -1,22 +1,22 @@
 1class Solution {
 2public:
 3    vector<int> dailyTemperatures(vector<int>& temperatures) {
-4       int n=temperatures.size();
-5       stack<int>st;
-6       vector<int>res(n);
-7       st.push(n-1);
-8       res[n-1]=0;
-9       for(int i=n-2;i>=0;i--){
-10        while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
-11            st.pop();
-12        }
-13        if(st.empty())res[i]=0;
-14        else{
-15            res[i]=st.top()-i;
-16        }
-17        st.push(i);
-18       }
-19       return res;
-20    }
-21};
-22 // we are going to use next greater element logic but we are storing indexes instead of actual value
+4       int n= temperatures.size();
+5           vector<int>ans(n,0);
+6           stack<int>st;
+7           st.push(n-1);
+8           for(int i=n-2;i>=0;i--){
+9            while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
+10                st.pop();
+11            }
+12            if(st.empty()){
+13                ans[i]=0;
+14            }
+15            else{
+16                ans[i]=st.top()-i;
+17            }
+18            st.push(i);
+19           }
+20return ans;
+21    }
+22};
