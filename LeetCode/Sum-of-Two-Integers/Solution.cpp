86e140ea0fe8f001b9f0d79026523a91ff724064
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-15 05:35:31 +0530

    Add solution for Sum of Two Integers

--- LeetCode/Sum-of-Two-Integers/Solution.cpp
+++ LeetCode/Sum-of-Two-Integers/Solution.cpp
@@ -5,10 +5,11 @@
 5        // (A&B)<<1 FOR THE CARRY BIT
 6
 7            while(a!=0){
-8           unsigned int carry=(unsigned int)(a&b)<<1;
-9            b=a^b;
-10            a=carry;
-11            }
-12            return b;
-13    }
-14};
+8        //    unsigned int carry=(unsigned int)(a&b)<<1;
+9           int carry=(a&b)<<1;
+10            b=a^b;
+11            a=carry;
+12            }
+13            return b;
+14    }
+15};
