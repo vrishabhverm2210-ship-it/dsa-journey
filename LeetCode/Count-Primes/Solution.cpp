1bc87dfd68fce433901c33f30254bcffd75cb308
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-16 05:06:42 +0530

    Add solution for Count Primes

--- LeetCode/Count-Primes/Solution.cpp
+++ LeetCode/Count-Primes/Solution.cpp
@@ -1,22 +1,26 @@
 1class Solution {
 2public:
 3    int countPrimes(int n) {
-4        int count=1;
-5        vector<bool>res(n,0);  // 0 means all prime
-6if(n <= 2) return 0;
-7       
-8        for(int i=3;i*i<n; i += 2){       
-9            if(res[i]==0){
-10            for(int j=i*i;j<n;j+=2*i){
-11                res[j]=1;
-12            }
-13        }
-14        }
-15        for(int i=3;i<res.size(); i += 2){
-16            if(res[i]==0){
-17                count++;
-18            }
-19        }
-20        return count;
-21    }
-22};
+4        int count=1; // except two all even numbers are comosite sono need to check them
+5        vector<bool>res(n,1);  // 1 means prime
+6        if(n==0||n==1)return 0;
+7        res[0]=0;
+8        res[1]=0;
+9        if(n==2)return 0;
+10        // res[2]=1;
+11        for(int i=3;i*i<n;i+=2){
+12            if(res[i]==1){
+13            for(int j=i*i;j<n;j=j+2*i){
+14
+15                res[j]=0;
+16            }
+17            }
+18        }
+19        for(int i=3;i<n;i+=2 ){
+20        if(res[i]==1){
+21            count++;
+22        }
+23        }
+24        return count;
+25    }
+26};
