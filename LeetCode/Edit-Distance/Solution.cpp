ee09bd4dcff88438a7e7b42f5f83fb031e4d65ef
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-15 06:50:50 +0530

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.cpp
+++ LeetCode/Edit-Distance/Solution.cpp
@@ -1,31 +1,34 @@
 1class Solution {
 2public:
-3    int minDistance(string word1, string word2) {
-4        // FIND LCS
-5        int n=word1.size();
-6        int m=word2.size();
-7        vector<vector<int>>dp(n+1);
-8        for(int i=0;i<=n;i++){
-9            vector<int>t(m+1,-1);
-10            dp[i]=t;
-11        }
-12        for(int i=0;i<=m;i++){
-13            dp[0][i]=i;
+3    int solveRec(int i, int j, string& word1, string& word2, vector<vector<int>>& dp) {
+4        // Base cases
+5        if (i == 0) return j; // Need to insert j characters
+6        if (j == 0) return i; // Need to delete i characters
+7
+8        // If already computed
+9        if (dp[i][j] != -1) return dp[i][j];
+10
+11        // If characters match, no operation needed
+12        if (word1[i - 1] == word2[j - 1]) {
+13            return dp[i][j] = solveRec(i - 1, j - 1, word1, word2, dp);
 14        }
-15        for(int i=0;i<=n;i++){
-16            dp[i][0]=i;
-17        }
-18        for(int i=1;i<=n;i++){
-19            for(int j=1;j<=m;j++){
-20            if(word1[i-1]==word2[j-1]){
-21                dp[i][j]=dp[i-1][j-1];
-22                
-23            }
-24            else{
-25                dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
-26            }
-27            }
-28        }
-29        return dp[n][m];
-30    }
-31};
+15
+16        // If characters don't match: try all 3 operations
+17        int insertOp = solveRec(i, j - 1, word1, word2, dp);     // Insert
+18        int deleteOp = solveRec(i - 1, j, word1, word2, dp);     // Delete
+19        int replaceOp = solveRec(i - 1, j - 1, word1, word2, dp); // Replace
+20
+21        return dp[i][j] = 1 + min({insertOp, deleteOp, replaceOp});
+22    }
+23
+24    int minDistance(string word1, string word2) {
+25        int n = word1.length();
+26        int m = word2.length();
+27
+28        // DP table initialized with -1
+29        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+30
+31        return solveRec(n, m, word1, word2, dp);
+32    }
+33};
+34
