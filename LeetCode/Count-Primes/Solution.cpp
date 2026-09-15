17cd26b3eae488195b667767c1fb87abae81ec4e
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-15 06:26:45 +0530

    Add solution for Count Primes

--- LeetCode/Count-Primes/Solution.cpp
+++ LeetCode/Count-Primes/Solution.cpp
@@ -5,7 +5,7 @@
 5        vector<bool>res(n,0);  // 0 means all prime
 6if(n <= 2) return 0;
 7       
-8        for(int i=3;1LL*i*i<n; i += 2){       
+8        for(int i=3;i*i<n; i += 2){       
 9            if(res[i]==0){
 10            for(int j=i*i;j<n;j+=2*i){
 11                res[j]=1;
