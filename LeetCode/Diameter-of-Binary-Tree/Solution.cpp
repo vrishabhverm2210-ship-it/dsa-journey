57a3322ed39912b2847c75e6ed598675bb4de981
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-16 06:46:02 +0530

    Add solution for Diameter of Binary Tree

--- LeetCode/Diameter-of-Binary-Tree/Solution.cpp
+++ LeetCode/Diameter-of-Binary-Tree/Solution.cpp
@@ -13,18 +13,15 @@
 13public:
 14int fun1(TreeNode* root,int &res){
 15    if(root==NULL)return 0;
-16    // basic idea is that we will calculate the max height of both left and right part and we will add it for the resultant diameter
-17    int left=fun1(root->left,res);
-18    int right=fun1(root->right,res);
-19    int total=left+right;
-20    res=max(res,total);
-21    return 1+max(left,right);
-22    
-23}
-24    int diameterOfBinaryTree(TreeNode* root) {
-25        int res=0;
-26         fun1(root,res);
-27         return res;
-28        
-29    }
-30};
+16    int left=fun1(root->left,res);
+17    int right=fun1(root->right,res);
+18            int total=left+right;
+19    res=max(res,total);
+20    return 1+max(left,right);
+21}
+22    int diameterOfBinaryTree(TreeNode* root) {
+23        int res=0;
+24      fun1(root,res);
+25        return res;
+26    }
+27};
