6a694773ed0b7a07378271037c8165ef75e95b9a
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-16 06:35:41 +0530

    Add solution for Next Greater Element I

--- LeetCode/Next-Greater-Element-I/Solution.cpp
+++ LeetCode/Next-Greater-Element-I/Solution.cpp
@@ -2,34 +2,43 @@
 2public:
 3    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 4        int n=nums1.size();
-5       
+5       vector<int>res;
 6         stack<int>st; 
 7          int m=nums2.size();
 8         st.push(nums2[m-1]);
-9         vector<int>nxt(m,-1);
-10         for(int i=m-2;i>=0;i--){
-11            while(!st.empty()&&nums2[i]>=st.top()){
-12             st.pop();
-13            }
-14            if(st.empty()){
-15                   st.push(nums2[i]);
-16                continue;
-17            }
-18            else{
-19              nxt[i]=st.top();
-20            }
-21           st.push(nums2[i]);
-22         }
-23         unordered_map<int,int>mpp1;
-24         for(int i=0;i<m;i++){
-25            mpp1[nums2[i]]=i;
-26         }
-27  vector<int>res(n,-1);
-28         for(int i=0;i<n;i++){
-29        if(mpp1.find(nums1[i])!=mpp1.end()){
-30res[i] = nxt[mpp1[nums1[i]]];        }
-31         }
-32         return res;
-33
-34    }
-35};
+9         unordered_map<int,int>mpp1;
+10         mpp1[nums2[m-1]] = -1;
+11        //  vector<int>nxt(m,-1);
+12        // unordered_map<int,int>mpp1;
+13         for(int i=m-2;i>=0;i--){
+14            while(!st.empty()&&nums2[i]>=st.top()){
+15             st.pop();
+16            }
+17            if(st.empty()){
+18                   st.push(nums2[i]);
+19                   mpp1[nums2[i]]=-1;
+20                continue;
+21            }
+22            else{
+23                mpp1[nums2[i]]=st.top();
+24            //   nxt[i]=st.top();
+25            }
+26           st.push(nums2[i]);
+27         }
+28       for(int i=0;i<n;i++){
+29        res.push_back(mpp1[nums1[i]]);
+30       }
+31       return res;
+32//          unordered_map<int,int>mpp1;
+33//          for(int i=0;i<m;i++){
+34//             mpp1[nums2[i]]=i;
+35//          }
+36//   vector<int>res(n,-1);
+37//          for(int i=0;i<n;i++){
+38//         if(mpp1.find(nums1[i])!=mpp1.end()){
+39//       res[i] = nxt[mpp1[nums1[i]]];        }
+40//          }
+41//          return res;
+42
+43    }
+44};
