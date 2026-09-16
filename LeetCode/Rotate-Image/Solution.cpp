a9c44d391db72741a52463ce75909994c6c004e0
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-16 06:06:32 +0530

    Add solution for Rotate Image

--- LeetCode/Rotate-Image/Solution.cpp
+++ LeetCode/Rotate-Image/Solution.cpp
@@ -2,25 +2,22 @@
 2public:
 3    void rotate(vector<vector<int>>& matrix) {
 4        int n=matrix.size();
-5        for(int i=0;i<n;i++){
-6            for(int j=i+1;j<n;j++){
-7                int temp=matrix[i][j];
-8                matrix[i][j]=matrix[j][i];
-9                matrix[j][i]=temp;
-10            }
-11        }
-12
-13        // now 
-14        int left=0;
-15        int right=n-1;
-16        while(left<right){
-17        for(int i=0;i<n;i++){
-18                int temp= matrix[i][left];
-19                matrix[i][left]=matrix[i][right];
-20                matrix[i][right]=temp;
+5        // firsty do the transpose
+6        for(int i=0;i<n;i++){
+7            for(int j=i+1;j<n;j++){
+8                 int temp=matrix[i][j];
+9                matrix[i][j]=matrix[j][i];
+10                matrix[j][i]=temp;
+11            }
+12        }
+13      int i=0;
+14      int j=n-1;
+15        while(i<j){
+16        for(int k=0;k<n;k++){
+17            swap(matrix[k][i] ,matrix[k][j]);
+18        }
+19        i++;
+20        j--;
 21        }
-22        left++;
-23        right--;
-24   }
-25    }
-26};
+22    }
+23};
