bfee5bf210e37702ef9647a4d4284a220ab9bed9
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-19 06:08:39 +0530

    Add solution for Excel Sheet Column Number

--- LeetCode/Excel-Sheet-Column-Number/Solution.cpp
+++ LeetCode/Excel-Sheet-Column-Number/Solution.cpp
@@ -6,7 +6,7 @@
 6        for(int i=0;i<n;i++){
 7
 8            int value = columnTitle[i] - 'A' + 1;
-9             ans=ans*26+value;
+9             ans=ans*26+value;    // 26 is the base unit
 10        }
 11        return ans;
 12    }
