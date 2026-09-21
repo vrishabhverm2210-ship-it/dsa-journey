3f00bf606b87c8e3fbb31962028912f727941f62
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-22 05:15:08 +0530

    Add solution for Next Greater Element II

--- LeetCode/Next-Greater-Element-II/Solution.cpp
+++ LeetCode/Next-Greater-Element-II/Solution.cpp
@@ -1,27 +1,24 @@
 1class Solution {
 2public:
 3    vector<int> nextGreaterElements(vector<int>& nums) {
-4        int n= nums.size();
-5        // push the choice into the stack 
-6        stack<int>st;
-7        for(int i=n-2;i>=0;i--){
-8            st.push(nums[i]);
-9        }
-10        vector<int>res;
-11        // now normal loop to find the next greater element
-12        for(int i=n-1;i>=0;i--){
-13            while(!st.empty() && st.top()<=nums[i]){
-14                st.pop();
-15            }
-16            if(st.empty()){
-17                res.push_back(-1);
-18            }
-19            else{
-20               res.push_back(st.top());
-21            }
-22            st.push(nums[i]);
-23        }
-24        reverse(res.begin(),res.end());
-25        return res;
-26    }
-27};
+4       stack<int>st;
+5       int n=nums.size();
+6       for(int i=n-2;i>=0;i--){   // we are pushing the elements for the last index , for providing him fair chance
+7        st.push(nums[i]);
+8       }
+9        vector<int>res(n);
+10       // now we have to apply are standard next greater template
+11       for(int i=n-1;i>=0;i--){ 
+12        // popping case
+13        while(!st.empty() && st.top()<=nums[i]){
+14            st.pop();
+15        }
+16        if(st.empty())res[i]=-1;
+17        else{
+18          res[i]=st.top();
+19        }
+20        st.push(nums[i]);
+21       }
+22       return res;
+23    }
+24};
