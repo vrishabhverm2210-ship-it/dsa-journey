78296cefe3e8d0e2aca84364cdb369ca20f33cc4
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-21 08:08:14 +0530

    Add solution for Find Peak Element

--- LeetCode/Find-Peak-Element/Solution.cpp
+++ LeetCode/Find-Peak-Element/Solution.cpp
@@ -1,25 +1,23 @@
 1class Solution {
 2public:
 3    int findPeakElement(vector<int>& nums) {
-4     // i have two conditions in my mind
-5     int low=0;
-6     int high=nums.size()-1;
-7     int res=-1;
-8     while(low<=high){
-9        int mid=low+(high-low)/2;
-10          if(mid == nums.size() - 1){
-11        res = mid;
-12        break;
-13    }
-14        if(nums[mid]<nums[mid+1]){
-15            low=mid+1;
-16        }
-17        else{
-18          
-19             res=mid;
-20            high=mid-1;
-21        }
-22     }   
-23     return res;
-24    }
-25};
+4        int low=0;
+5        int high=nums.size()-1;
+6        int res=-1;
+7        while(low<=high){
+8            int mid=low+(high-low)/2;
+9           if(mid==nums.size()-1){
+10            res = mid;
+11            break;
+12           }
+13           else if(nums[mid]<nums[mid+1]){
+14            low=mid+1;
+15           }
+16           else{
+17            res=mid;
+18            high=mid-1;
+19           }
+20        }
+21return res;
+22    }
+23};
