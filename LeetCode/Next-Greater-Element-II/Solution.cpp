4931a9a45e264659132f662f3203f41719186737
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-22 05:14:40 +0530

    Add solution for Next Greater Element II

--- LeetCode/Next-Greater-Element-II/Solution.cpp
+++ LeetCode/Next-Greater-Element-II/Solution.cpp
@@ -1,23 +1,27 @@
-Stack<Integer> stack = new Stack<>();
-
-public int[] nextGreaterElements(int[] nums) {
-
-    int[] res = new int[nums.length];
-    for(int i=0;i<nums.length;i++) {
-        while(!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-            res[stack.pop()] = nums[i];
-        }
-        stack.push(i);
-    }
-    
-    for(int i=0;i<nums.length;i++) {
-        while(!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-            res[stack.pop()] = nums[i];
-        }
-    }
-
-    while(!stack.isEmpty()) {
-        res[stack.pop()] = -1;
-    }
-    return res;
-}
+1class Solution {
+2public:
+3    vector<int> nextGreaterElements(vector<int>& nums) {
+4        int n= nums.size();
+5        // push the choice into the stack 
+6        stack<int>st;
+7        for(int i=n-2;i>=0;i--){
+8            st.push(nums[i]);
+9        }
+10        vector<int>res;
+11        // now normal loop to find the next greater element
+12        for(int i=n-1;i>=0;i--){
+13            while(!st.empty() && st.top()<=nums[i]){
+14                st.pop();
+15            }
+16            if(st.empty()){
+17                res.push_back(-1);
+18            }
+19            else{
+20               res.push_back(st.top());
+21            }
+22            st.push(nums[i]);
+23        }
+24        reverse(res.begin(),res.end());
+25        return res;
+26    }
+27};
