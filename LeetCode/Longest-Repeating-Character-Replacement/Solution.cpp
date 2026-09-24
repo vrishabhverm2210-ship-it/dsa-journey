18eb8f53d2cb65455e948b12c7328b2705820c96
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-24 05:54:52 +0530

    Add solution for Longest Repeating Character Replacement

--- LeetCode/Longest-Repeating-Character-Replacement/Solution.cpp
+++ LeetCode/Longest-Repeating-Character-Replacement/Solution.cpp
@@ -1,27 +1,30 @@
 1class Solution {
 2public:
-3int maxcnt(vector<int>& freq){
+3int fun1(   unordered_map<char,int>&mpp1){
 4    int maxi=INT_MIN;
-5    for(int i=0;i<26;i++){
-6        maxi=max(maxi,freq[i]);
+5    for(auto i:mpp1){
+6        maxi=max(maxi,i.second);
 7    }
 8    return maxi;
 9}
 10    int characterReplacement(string s, int k) {
-11        // let;s build vector instead of map now because we have to keep the track of the count also
-12    vector<int> freq(26, 0); // for frequencies and also there are only the uppercase letters
-13    int low=0;
-14    int res=0;
-15    for(int high=0;high<s.size();high++){
-16        freq[s[high]- 'A']++;    // new thing  
-17        // shrinking phase
-18        while((high-low+1)-maxcnt(freq) > k){
-19             freq[s[low]-'A']--;
-20             low++;
-21        }
-22        // update the res
-23        res=max(res,high-low+1);
-24    }
-25return res;
-26    }
-27};
+11        int n=s.size();
+12        int low=0;
+13        int high=0;
+14        int res=-1;
+15        unordered_map<char,int>mpp1;
+16        for(high=0;high<n;high++){
+17            mpp1[s[high]]++;
+18            // wrong block
+19            while((high-low+1)-fun1(mpp1)>k){
+20               mpp1[s[low]]--;
+21               if(mpp1[s[low]]==0)mpp1.erase(s[low]);
+22               low++;
+23            }
+24
+25int len=high-low+1;
+26res=max(res,len);
+27        }
+28        return res;
+29    }
+30};
