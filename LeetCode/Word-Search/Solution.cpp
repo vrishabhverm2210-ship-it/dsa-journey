0213f3bca12c7860ebbd8e6aa47c844ef474f5f6
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-24 07:20:09 +0530

    Add solution for Word Search

--- LeetCode/Word-Search/Solution.cpp
+++ LeetCode/Word-Search/Solution.cpp
@@ -1,59 +1,63 @@
 1class Solution {
 2public:
-3
-4    int x[4] = {-1, 1, 0, 0};
-5    int y[4] = {0, 0, -1, 1};
-6
-7    bool isvalid(int i, int j, vector<vector<char>>& board) {
-8        return i >= 0 && i < board.size() &&
-9               j >= 0 && j < board[0].size() &&
+3    bool isfound = false;
+4
+5    int x[4] = {-1, 1, 0, 0};
+6    int y[4] = {0, 0, -1, 1};
+7
+8    bool isvalid(int i, int j, vector<vector<char>>& board) {
+9        return i >= 0 && i < board.size() && j >= 0 && j < board[0].size() &&
 10               board[i][j] != '-1';
 11    }
 12
-13    bool dfs(vector<vector<char>>& board, string& word,
-14             int idx, int i, int j) {
-15
-16        if (idx == word.size() - 1)
-17            return true;
-18
-19        char original = board[i][j];
-20        board[i][j] = '-1';
+13    void dfs(vector<vector<char>>& board, string& word, int idx, int i, int j) {
+14
+15        if (idx == word.size() - 1) {
+16            isfound = true;
+17            return;
+18        }
+19
+20        char original = board[i][j];
 21
-22        for (int k = 0; k < 4; k++) {
-23
-24            int row = i + x[k];
-25            int col = j + y[k];
+22        // mark visited
+23        board[i][j] = '-1';
+24
+25        for (int k = 0; k < 4; k++) {
 26
-27            if (isvalid(row, col, board) &&
-28                board[row][col] == word[idx + 1]) {
+27            int row = i + x[k];
+28            int col = j + y[k];
 29
-30                if (dfs(board, word, idx + 1, row, col))
-31                    return true;
-32            }
-33        }
-34
-35        // BACKTRACK
-36        board[i][j] = original;
-37
-38        return false;
-39    }
-40
-41    bool exist(vector<vector<char>>& board, string word) {
+30            if (isvalid(row, col, board) && board[row][col] == word[idx + 1]) {
+31
+32                dfs(board, word, idx + 1, row, col);
+33
+34                if (isfound)
+35                    return;
+36            }
+37        }
+38
+39        // backtrack
+40        board[i][j] = original;
+41    }
 42
-43        int n = board.size();
-44        int m = board[0].size();
-45
-46        for (int i = 0; i < n; i++) {
-47            for (int j = 0; j < m; j++) {
-48
-49                if (board[i][j] == word[0]) {
+43    bool exist(vector<vector<char>>& board, string word) {
+44
+45        int n = board.size();
+46        int m = board[0].size();
+47
+48        for (int i = 0; i < n; i++) {
+49            for (int j = 0; j < m; j++) {
 50
-51                    if (dfs(board, word, 0, i, j))
-52                        return true;
-53                }
-54            }
-55        }
-56
-57        return false;
-58    }
-59};
+51                if (board[i][j] == word[0]) {
+52
+53                    dfs(board, word, 0, i, j);
+54
+55                    if (isfound)
+56                        return true;
+57                }
+58            }
+59        }
+60
+61        return false;
+62    }
+63};
