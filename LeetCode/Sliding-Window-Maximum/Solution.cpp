140fd7ddccbde53ebc23980d458bd474a10de36f
vrishabhverm2210-ship-it <vrishabhverm2210@gmail.com>
2026-09-26 16:55:56 +0530

    Add solution for Sliding Window Maximum

--- LeetCode/Sliding-Window-Maximum/Solution.cpp
+++ LeetCode/Sliding-Window-Maximum/Solution.cpp
@@ -1,27 +1,24 @@
 1class Solution {
 2public:
 3    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-4        int n=nums.size();
-5    vector<int>res;
-6    deque<int>dq;
-7    for(int i=0;i<n;i++)
-8    {
-9      // size se jada toh ni ho gye element
-10      if(!dq.empty() && dq.front() <=i-k){
-11        dq.pop_front();
-12      }
-13
-14      // now if current element se  chote element hai dq mai (we have to maintain the decreeassing order montonic stack)
-15      while(!dq.empty() && nums[dq.back()]<=nums[i]){
-16        dq.pop_back();
-17      }
-18// current element daal do
-19      dq.push_back(i);
-20      // check first window puri hui ya fir nahi
-21      if(i >= k-1){
-22        res.push_back(nums[dq.front()]);
-23      }
-24    }
-25    return res;
-26    }
-27};
+4        vector<int>res;
+5        int n=nums.size();
+6        deque<int>dq;
+7        for(int i=0;i<n;i++){
+8              // size se jada toh ni ho gye element
+9           if(!dq.empty() && dq.front() <=i-k){
+10               dq.pop_front();
+11            }
+12          while(!dq.empty()&& nums[i]>= nums[dq.back()]){    
+13               dq.pop_back();
+14            }
+15  // current element daal do
+16      dq.push_back(i);
+17
+18    if(i>=k-1){
+19        res.push_back(nums[dq.front()]);
+20    }
+21        }
+22        return res;
+23    }
+24};
